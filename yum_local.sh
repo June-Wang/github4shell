a1b3db64650e6b2dd4d6ef6c637864a2035457f5
wang xiaojun <wang_xj@suixingpay.com>
2013-01-29 11:42:47 +0800

    modify yum_local.sh

--- yum_local.sh
+++ yum_local.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-yum_server='yum.suixingpay.com'
-
 check_system (){
 system_info=`head -n 1 /etc/issue`
 case "${system_info}" in
         'CentOS release 5'*) 
 		system='centos5'
 		yum_source_name='centos5-lan'
-		file='/etc/yum.repos.d/CENTOS5-lan.repo'
+		repo_file='/etc/yum.repos.d/CENTOS5-lan.repo'
 		;;
         'Red Hat Enterprise Linux Server release 5'*) 
 		system='rhel5'
 		yum_source_name='RHEL5-lan'
-		file='/etc/yum.repos.d/RHEL5-lan.repo'
+		repo_file='/etc/yum.repos.d/RHEL5-lan.repo'
 		;;
         *) 
 		system='unknown'
@@ -23,16 +21,19 @@ case "${system_info}" in
 esac
 }
 
-check_system
-
-grep -E "${yum_source_name}" >/dev/null 2>&1 ${file} && set_yum='ok'
+check_repo_config () {
+#Detect repo configuration
+grep -E "${yum_source_name}" >/dev/null 2>&1 ${repo_file} && set_yum='ok'
 [ "${set_yum}" = 'ok' ] && exit 0
+}
 
-echo "set yum please wait ......"
+check_platform () {
 platform_info=`uname -m`
 echo ${platform_info}|grep '64' >/dev/null 2>&1 && platform='x64' || platform='x86'
+}
 
-profile_dir='/etc/profile.d'
+set_repo_file () {
+#check operating system and set yum local repo 
 case "${system}" in
 rhel5)
 echo "[RHEL5-lan]
@@ -47,11 +48,46 @@ baseurl=http://${yum_server}/centos5.8_${platform}/
 gpgcheck=0" > ${file}
 ;;
 esac
+}
 
-[ -d "${profile_dir}" ] && echo "alias yum='yum --skip-broken --nogpgcheck --disablerepo=\* --enablerepo=${yum_source_name}'" > ${profile_dir}/yum_alias.sh
+alias_yum () {
+profile_dir='/etc/profile.d'
+[ -d "${profile_dir}" ] &&\
+echo "alias yum='yum --skip-broken --nogpgcheck --disablerepo=\* --enablerepo=${yum_source_name}'" > ${profile_dir}/yum_alias.sh
 alias yum="yum --skip-broken --nogpgcheck --disablerepo=\* --enablerepo=${yum_source_name}"
+}
 
-if [ -e /etc/hosts ];then
-        grep 'yum.suixingpay.com' /etc/hosts >/dev/null 2>&1 || echo '192.168.29.234 yum.suixingpay.com' >> /etc/hosts
+set_dns_server () {
+dns_config='/etc/resolv.conf'
+if [ -e ${dns_config} ];then
+        grep '192.168.29.230' ${dns_config} >/dev/null 2>&1 || echo 'nameserver 192.168.29.230' >> ${dns_config}
 fi
+}
+
+set_yum_proxy () {
+yum_config='/etc/yum.conf'
+if [ -e ${yum_config} ];then
+	grep -E '^proxy*' ${yum_config} >/dev/null 2>&1 &&\
+	sed -r -i "s/^proxy*/proxy = http:\/\/${yum_server}:80\//" ${yum_config} ||\
+	echo "proxy=http://${yum_server}:80/" >> ${yum_config}
+fi
+}
+
+create_yum_cache () {
 yum makecache |tail -n 1 && exit 0
+}
+
+main () {
+yum_server='yum.suixingpay.com'
+check_system
+check_repo_config
+check_platform
+echo "Set yum please wait ......"
+set_repo_file
+alias_yum
+set_dns_server
+set_yum_proxy
+create_yum_cache
+}
+
+main
