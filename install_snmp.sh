d3177117ca2352fd8530b858c15c905f877fe730
wang xiaojun <wang_xj@suixingpay.com>
2013-07-15 17:04:35 +0800

    modify install_snmp.sh

--- install_snmp.sh
+++ install_snmp.sh
@@ -93,9 +93,9 @@ com2sec notConfigUser  default       ${community}
 group   notConfigGroup v1           notConfigUser
 group   notConfigGroup v2c           notConfigUser
 view    systemview    included   .1
-access  notConfigGroup ""      any       noauth    exact  systemview none none
+access  notConfigGroup \"\"      any       noauth    exact  systemview none none
 syslocation ${location}
-syscontact ${contact} ${email}
+syscontact ${contact} (${email})
 dontLogTCPWrappersConnects yes
 #SET SNMP _END_" >${snmp_conf}
 
