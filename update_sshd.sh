37920156c260d66a6c5416aed29cf97841f20d21
wang xiaojun <wang_xj@suixingpay.com>
2013-08-08 14:42:40 +0800

    add update_sshd.sh

---
+++ update_sshd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum_server='yum.suixingpay.com'
+
+pkgs=(
+'openssh-6.1p1-5.el5.1.x86_64.rpm'
+'openssh-server-6.1p1-5.el5.1.x86_64.rpm'
+'openssh-clients-6.1p1-5.el5.1.x86_64.rpm'
+'libedit0-3.0-1.20090722cvs.el5.x86_64.rpm'
+)
+
+for pkg in "${pkgs[@]}"
+do
+	wget http://${yum_server}/tools/${pkg} -O /tmp/${pkg} || eval "echo Can not download ${pkg}!;exit 1"
+done
+
+cd /tmp/
+
+rpm -Uvh ${pkgs[@]} && rm -f ${pkgs[@]}
