0d9388307cccea452f2f81bcfafa03760f411731
wang xiaojun <wang_xj@suixingpay.com>
2013-11-05 15:04:57 +0800

    modify install_mawk.sh

--- install_mawk.sh
+++ install_mawk.sh
@@ -1,81 +1,112 @@
 #!/bin/bash
 
-init_var () {
-yum_server="$1"
-file_name="$2"
-yum_para="$3"
-if [ "${yum_para}" = 'lan' ];then
-	YUM='yum --disablerepo=\* --enablerepo=centos5-lan'
-else
-	YUM='yum'
+#SET TEMP PATH
+TEMP_PATH='/usr/local/src'
+
+#SET GLOBAL VAR
+MY_PROJECT='mawk'
+PACKAGE='mawk-1.3.4.tar.gz'
+YUM_SERVER='yum.suixingpay.com'
+YUM_PACKAGE='gcc glibc glibc-common make cmake gcc-c++'
+APT_PACKAGE='build-essential'
+
+check_system (){
+SYSTEM_INFO=`head -n 1 /etc/issue`
+case "${SYSTEM_INFO}" in
+        'CentOS release 5'*)
+                SYSTEM='centos5'
+                YUM_SOURCE_NAME='centos5-lan'
+		        CONFIG_CMD='chkconfig'
+                ;;
+        'Red Hat Enterprise Linux Server release 5'*)
+                SYSTEM='rhel5'
+                YUM_SOURCE_NAME='RHEL5-lan'
+        		CONFIG_CMD='chkconfig'
+                ;;
+	    'Debian GNU/Linux 6'*)
+    		    SYSTEM='debian6'
+        		CONFIG_CMD='sysv-rc-conf'
+                ;;
+        *)
+                SYSTEM='unknown'
+                echo "This script not support ${SYSTEM_INFO}" 1>&2
+                exit 1
+                ;;
+esac
+}
+
+set_install_cmd () {
+local para="$1"
+case "${SYSTEM}" in
+    centos5|rhel5)
+        local install_cmd='yum --skip-broken --nogpgcheck'
+        local package="${YUM_PACKAGE}"
+    ;;
+    debian6)
+        local install_cmd='apt-get --force-yes'
+        local package="${APT_PACKAGE}"
+        eval "${install_cmd} install -y sysv-rc-conf >/dev/null 2>&1" || eval "echo ${install_cmd} fail! 1>&2;exit 1"
+    ;;
+    *)
+        echo "This script not support ${SYSTEM_INFO}" 1>&2
+                exit 1
+        ;;
+esac
+
+if [ "${install_cmd}" = 'yum' -a "${para}" = 'lan' ];then
+        install_cmd="yum --skip-broken --nogpgcheck --disablerepo=\* --enablerepo=${YUM_SOURCE_NAME}"
 fi
-package_url="http://${yum_server}/tools/${file_name}"
-}
 
-create_user () {
-	username="$1"
-	grep "${username}" /etc/passwd >/dev/null 2>&1 || useradd  -c "${username} user" -s /sbin/nologin ${username}
-}
+local log_file="${TEMP_PATH}/yum_for_${MY_PROJECT}.log"
 
-install_lib () {
-log_name="$1"
-log_file="${local_path}/yum_for_${log_name}.log"
-echo -n "install gcc gcc-c++ make please wait ...... "
-eval "${YUM} install -y gcc gcc-c++ make >${log_file} 2>&1" || yum_install='fail'
-if [ "${yum_install}" = "fail" ];then
-        echo -e "yum not available!\nview error please type: less ${log_file}" 1>&2
+echo -n "install ${package} please wait ...... "
+eval "${install_cmd} install -y ${package} >${log_file} 2>&1" || local install_stat='fail'
+if [ "${install_stat}" = "fail" ];then
+        echo -e "${install_cmd} not available!\nview error please type: less ${log_file}" 1>&2
         exit 1
 fi
 echo "done."
 }
 
-make_tmp_dir () {
-mkdir -p "${local_path}/${install_dir}" && cd "${local_path}/${install_dir}" || mkdir_dir='fail'
+create_tmp_dir () {
+mkdir -p "${INSTALL_PATH}" && cd "${INSTALL_PATH}" || local mkdir_dir='fail'
 if [ "${mkdir_dir}" = "fail"  ];then
-        echo "mkdir ${install_dir} fail!" 1>&2
+        echo "mkdir ${INSTALL_PATH} fail!" 1>&2
         exit 1
 fi
 }
 
 del_tmp () {
-test -d "${local_path}/${install_dir}" && rm -rf "${local_path}/${install_dir}"
+test -d "${INSTALL_PATH}" && rm -rf "${INSTALL_PATH}"
 }
 
-check_urls () {
-for url in "$@"
-do
-        file=`echo ${url}|awk -F'/' '{print $NF}'`
-        if [ ! -f "${file}" ]; then
-                echo -n "download ${url} ...... "
-                wget -q "${url}"  && echo 'done.' || download='fail'
-                if [ "${download}" = "fail" ];then
-                        echo "download ${url} fail!" 1>&2 && del_tmp
-                        exit 1
-                fi
+download_file () {
+local   url="$1"
+local   file=`echo ${url}|awk -F'/' '{print $NF}'`
+
+if [ ! -f "${file}" ]; then
+        echo -n "download ${url} ...... "
+        wget -q "${url}"  && echo 'done.' || local download='fail'
+        if [ "${download}" = "fail" ];then
+                echo "download ${url} fail!" 1>&2 && del_tmp
+                exit 1
         fi
-done
+fi
 }
 
-install_pre () {
-        install_url="$1"
-        file=`echo ${install_url}|awk -F'/' '{print $NF}'`
-        dir=`echo ${file}|awk -F'.tar|.tgz' '{print $1}'`
-        test -e "${file}" && tar xzf ${file} || tar_file='not_exist'
-        cd ${dir} || file_dir='not_exist'
-        if [ "${tar_file}" = 'not_exist' ];then
-                echo "${file} not exist!" 1>&2 && del_tmp
-                exit 1
-        fi
-        if [ "${file_dir}" = 'not_exist' ];then
-                echo "plesse check ${file}!" 1>&2 && del_tmp
-                exit 1
-        fi
-        echo -n "Compile ${dir} please wait ...... "
+check_file () {
+local file="$1"
+local ex_dir=`echo "${file}"|awk -F'.tar|.tgz' '{print $1}'`
+local dir="${INSTALL_PATH}/${ex_dir}"
+
+test -f ${file} && tar xzf ${file} || eval "echo ${file} not exsit!;del_tmp;exit 1"
+test -d ${dir} && cd ${dir} || eval "echo ${dir} not exsit!;del_tmp;exit 1"
+echo -n "Compile ${file} please wait ...... "
 }
 
 run_cmds () {
-        cmd_log="${local_path}/install_${dir}.log"
-        test -f "${cmd_log}" && cat /dev/null > "${local_path}/install_${dir}.log"
+local   cmd_log="${TEMP_PATH}/install_${PACKAGE}.log"
+        test -f "${cmd_log}" && rm -f ${cmd_log}
         for cmd in "$@"
         do
                 ${cmd} >> "${cmd_log}" 2>&1 || compile='fail'
@@ -88,31 +119,33 @@ run_cmds () {
 }
 
 install_mawk () {
-	install_pre "${package_url}"
-	run_cmds './configure' 'make' 'make check' 'make install'
-	cd ..
+        download_file "${PACKAGE_URL}"
+        check_file "${PACKAGE}"
+        run_cmds './configure' 'make' 'make install'
+        cd ..
 }
 
 echo_bye () {
-	program="$1"
-	echo "Install ${program} complete!" && exit 0
+        echo "Install ${PACKAGE} complete!" && exit 0
 }
 
 main () {
-my_project='mawk'
-init_var 'yum.suixingpay.com' 'mawk-1.3.4-20120627.tgz' 'lan'
-install_lib "${my_project}"
-make_tmp_dir
-#create_user "${my_project}"
-check_urls "${package_url}"
+INSTALL_DIR="install_$$"
+INSTALL_PATH="${TEMP_PATH}/${INSTALL_DIR}"
+PACKAGE_URL="http://${YUM_SERVER}/tools/${PACKAGE}"
+
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap "rm -rf ${INSTALL_PATH}"  EXIT
+
+check_system
+#create_user
+create_tmp_dir
+#set_yum 'lan'
+set_install_cmd 'lan'
 install_mawk
+#set_auto_run
 del_tmp
-echo_bye "${my_project}"
+echo_bye
 }
 
-#local install path
-local_path='/usr/local/src'
-install_dir="install_$$"
-trap "exit 1"           HUP INT PIPE QUIT TERM
-trap "rm -f ${install_dir}"  EXIT
 main
