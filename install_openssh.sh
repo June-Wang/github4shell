6181b17de77357173fdea50f5f18bb890e55407b
wang xiaojun <wang_xj@suixingpay.com>
2013-11-12 13:51:53 +0800

    modify install_coreutils.sh

--- install_openssh.sh
+++ install_openssh.sh
@@ -6,20 +6,20 @@ case "${SYSTEM_INFO}" in
         'CentOS release 5'*)
                 SYSTEM='centos5'
                 YUM_SOURCE_NAME='centos5-lan'
-		CONFIG_CMD='chkconfig'
+                CONFIG_CMD='chkconfig'
                 ;;
         'Red Hat Enterprise Linux Server release 5'*)
                 SYSTEM='rhel5'
                 YUM_SOURCE_NAME='RHEL5-lan'
-        	CONFIG_CMD='chkconfig'
+                CONFIG_CMD='chkconfig'
                 ;;
-#	'Debian GNU/Linux 6'*)
-#		SYSTEM='debian6'
-#		CONFIG_CMD='sysv-rc-conf'
+#        'Debian GNU/Linux 6'*)
+#                SYSTEM='debian6'
+#                CONFIG_CMD='sysv-rc-conf'
 #                ;;
-#	'Debian GNU/Linux 7'*)
-#		SYSTEM='debian7'
-#		CONFIG_CMD='sysv-rc-conf'
+#        'Debian GNU/Linux 7'*)
+#                SYSTEM='debian7'
+#                CONFIG_CMD='sysv-rc-conf'
 #                ;;
         *)
                 SYSTEM='unknown'
@@ -107,7 +107,7 @@ local   cmd_log="${TEMP_PATH}/install_${PACKAGE}.log"
                 fi
         done
         echo "done."
-#		cd ..
+#               cd ..
 }
 
 download_and_check () {
@@ -120,8 +120,8 @@ echo_bye () {
 }
 
 exit_and_clear () {
-		del_tmp
-		echo_bye
+                del_tmp
+                echo_bye
 }
 
 main () {
@@ -130,7 +130,7 @@ TEMP_PATH='/usr/local/src'
 
 #SET PACKAGE
 YUM_SERVER='yum.suixingpay.com'
-YUM_PACKAGE='gcc glibc glibc-common make cmake gcc-c++ readline-devel'
+YUM_PACKAGE='gcc glibc glibc-common make cmake gcc-c++'
 APT_PACKAGE='build-essential'
 PACKAGE_URL="http://${YUM_SERVER}/tools"
 
@@ -147,14 +147,44 @@ check_system
 #create_tmp_dir
 set_install_cmd 'lan'
 
-#Install coreutils-8.21
-PACKAGE='coreutils-8.21.tar.gz'
+#Stop sshd service and clear old version
+SSH_SERVICE="/etc/init.d/sshd"
+test -f ${SSH_SERVICE} && ${SSH_SERVICE} stop
+rpm -qa openssh*|xargs -r -i rpm -e "{}"
+
+#Install zlib-1.2.8
+PACKAGE='zlib-1.2.8.tar.gz'
 create_tmp_dir
 download_and_check
-run_cmds 'export FORCE_UNSAFE_CONFIGURE=1' './configure --prefix=/' 'make' 'make install'
-
+run_cmds './configure' 'make' 'make install'
 #EXIT AND CLEAR TEMP DIR
 exit_and_clear
+
+#Install openssl-0.9.8y
+PACKAGE='openssl-0.9.8y.tar.gz'
+create_tmp_dir
+download_and_check
+run_cmds './config' 'make' 'make install'
+#EXIT AND CLEAR TEMP DIR
+exit_and_clear
+
+#install
+PACKAGE='openssh-6.4p1.tar.gz'
+create_tmp_dir
+download_and_check
+run_cmds './configure --prefix=/usr --sysconfdir=/etc/ssh' 'make' 'make install' 'cp contrib/redhat/sshd.init /etc/init.d/sshd'
+#EXIT AND CLEAR TEMP DIR
+exit_and_clear
+
+#Modify sshd_config
+SSH_CONFIG="/etc/ssh/sshd_config"
+test -f ${SSH_CONFIG} && sed -r -i 's/^(GSSAPI*)/#\1/g;s/^(UsePAM*)/#\1/g;s/^(UseDNS*)/#\1/g' ${SSH_CONFIG} ||\
+eval "echo ${SSH_CONFIG} not found!;exit 1"
+echo "UseDNS no" >> ${SSH_CONFIG}
+
+#Restart sshd service
+test -f ${SSH_SERVICE} && ${SSH_SERVICE} start
+${CONFIG_CMD} sshd on
 
 }
 
