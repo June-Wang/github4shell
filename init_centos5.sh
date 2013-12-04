906e2bf83ffffbd83d97ca227546c96a41a6baec
wang xiaojun <wang_xj@suixingpay.com>
2013-12-04 08:40:15 +0800

    关闭调整linux内核参数的初始化

--- init_centos5.sh
+++ init_centos5.sh
@@ -77,25 +77,25 @@ if [ -f "${sysctl_cf}" ];then
 	if [ "${sysctl_init}" = 'fail' ]; then 
 		/sbin/sysctl -a > /etc/sysctl.conf.${mydate}
 		echo '#init _BEGIN_
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_tw_recycle = 1
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_keepalive_time = 300
-net.ipv4.ip_local_port_range = 4000    65000
-net.ipv4.tcp_max_tw_buckets = 36000
-net.ipv4.route.gc_timeout = 100
-net.ipv4.tcp_syn_retries = 2
-net.ipv4.tcp_synack_retries = 2
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
-net.ipv4.tcp_rmem = 4096 87380 16777216
-net.ipv4.tcp_wmem = 4096 65536 16777216
-net.core.netdev_max_backlog = 30000
-net.ipv4.tcp_no_metrics_save = 1
-net.core.somaxconn = 262144
-net.ipv4.tcp_max_orphans = 262144
-net.ipv4.tcp_max_syn_backlog = 262144
+#net.ipv4.tcp_fin_timeout = 30
+#net.ipv4.tcp_tw_reuse = 1
+#net.ipv4.tcp_tw_recycle = 1
+#net.ipv4.tcp_syncookies = 1
+#net.ipv4.tcp_keepalive_time = 300
+#net.ipv4.ip_local_port_range = 4000    65000
+#net.ipv4.tcp_max_tw_buckets = 36000
+#net.ipv4.route.gc_timeout = 100
+#net.ipv4.tcp_syn_retries = 2
+#net.ipv4.tcp_synack_retries = 2
+#net.core.rmem_max = 16777216
+#net.core.wmem_max = 16777216
+#net.ipv4.tcp_rmem = 4096 87380 16777216
+#net.ipv4.tcp_wmem = 4096 65536 16777216
+#net.core.netdev_max_backlog = 30000
+#net.ipv4.tcp_no_metrics_save = 1
+#net.core.somaxconn = 262144
+#net.ipv4.tcp_max_orphans = 262144
+#net.ipv4.tcp_max_syn_backlog = 262144
 #SET sysctl.conf _END_' >> ${sysctl_cf}
 		/sbin/sysctl -p > ~/set_sysctl.log 2>&1
 		echo "sysctl set OK!!"
