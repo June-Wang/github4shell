c44861a434653beb7b9d6859a2b8169fbd28e8d0
wang xiaojun <wang_xj@suixingpay.com>
2013-12-17 09:45:52 +0800

    启用sshd的PAM

--- install_openssh.sh
+++ install_openssh.sh
@@ -178,10 +178,9 @@ exit_and_clear
 
 #Modify sshd_config
 SSH_CONFIG="/etc/ssh/sshd_config"
-#test -f ${SSH_CONFIG} && sed -r -i 's/^(GSSAPI*)/#\1/g;s/^(UsePAM*)/#\1/g;s/^(UseDNS*)/#\1/g' ${SSH_CONFIG} ||\
-test -f ${SSH_CONFIG} && sed -r -i 's/^(GSSAPI*)/#\1/g;s/^(UseDNS*)/#\1/g' ${SSH_CONFIG} ||\
+test -f ${SSH_CONFIG} && sed -r -i 's/^(GSSAPI*)/#\1/g;s/^(UsePAM*)/#\1/g;s/^(UseDNS*)/#\1/g' ${SSH_CONFIG} ||\
 eval "echo ${SSH_CONFIG} not found!;exit 1"
-echo "UseDNS no" >> ${SSH_CONFIG}
+echo -en "UseDNS no\nUsePAM yes\n" >> ${SSH_CONFIG}
 
 SSH_CONFIG="/etc/ssh/ssh_config"
 test -f ${SSH_CONFIG} && sed -i 's/GSSAPIAuthentication/#GSSAPIAuthentication/;s/^Host/#Host/' ${SSH_CONFIG} ||\
