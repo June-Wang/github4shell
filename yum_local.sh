682921b53689e14539927fad4127bcbbd01984d5
wang xiaojun <wang_xj@suixingpay.com>
2014-04-02 14:37:12 +0800

    添加对rhel6系列支持

--- yum_local.sh
+++ yum_local.sh
@@ -4,20 +4,25 @@ check_system (){
 system_info=`head -n 1 /etc/issue`
 case "${system_info}" in
         'CentOS release 5'*) 
-		system='centos5'
-		yum_source_name='centos5-lan'
-		repo_file='/etc/yum.repos.d/CENTOS5-lan.repo'
-		;;
+                system='centos5'
+                yum_source_name='centos5-lan'
+                repo_file='/etc/yum.repos.d/CENTOS5-lan.repo'
+                ;;
         'Red Hat Enterprise Linux Server release 5'*) 
-		system='rhel5'
-		yum_source_name='RHEL5-lan'
-		repo_file='/etc/yum.repos.d/RHEL5-lan.repo'
-		;;
+                system='rhel5'
+                yum_source_name='RHEL5-lan'
+                repo_file='/etc/yum.repos.d/RHEL5-lan.repo'
+                ;;
+        'Red Hat Enterprise Linux Server release 6'*) 
+                system='rhel6'
+                yum_source_name='RHEL6-lan'
+                repo_file='/etc/yum.repos.d/RHEL6-lan.repo'
+                ;;
         *) 
-		system='unknown'
-		echo "This script not support ${system_info}" 1>&2
-		exit 1
-		;;
+                system='unknown'
+                echo "This script not support ${system_info}" 1>&2
+                exit 1
+                ;;
 esac
 }
 
@@ -25,7 +30,8 @@ backup_local_repo_file () {
 local my_date=`date -d "now" +"%F"`
 SOURCE_DIR='/etc/yum.repos.d'
 if [ -d "${SOURCE_DIR}" ];then
-        find ${SOURCE_DIR} -type f -name "*.repo"|grep -Ev 'CENTOS5-lan.repo|RHEL5-lan.repo'|\
+#        find ${SOURCE_DIR} -type f -name "*.repo"|grep -Ev 'CENTOS5-lan.repo|RHEL5-lan.repo'|\
+        find ${SOURCE_DIR} -type f -name "*.repo"|\
         while read source_file
         do
                 mv "${source_file}" "${source_file}.${my_date}.$$"
@@ -50,13 +56,19 @@ case "${system}" in
 rhel5)
 echo "[RHEL5-lan]
 name=Red Hat Enterprise Linux \$releasever - \$basearch
-baseurl=http://${yum_server}/rhel5.8_${platform}/Server/
+baseurl=http://${yum_server}/rhel/5.8/${platform}/Server/
+gpgcheck=0" > ${repo_file}
+;;
+rhel6)
+echo "[RHEL6-lan]
+name=Red Hat Enterprise Linux \$releasever - \$basearch
+baseurl=http://${yum_server}/rhel/6.8/${platform}/Server/
 gpgcheck=0" > ${repo_file}
 ;;
 centos5)
 echo "[centos5-lan]
 name=CentOS-\$releasever - Media
-baseurl=http://${yum_server}/centos5.8_${platform}/
+baseurl=http://${yum_server}/centos/5.8/${platform}/
 gpgcheck=0" > ${repo_file}
 ;;
 esac
@@ -75,16 +87,16 @@ alias yum="${yum_para}"
 set_dns_server () {
 dns_config='/etc/resolv.conf'
 if [ -e ${dns_config} ];then
-        grep '192.168.29.230' ${dns_config} >/dev/null 2>&1 || echo 'nameserver 192.168.29.230' >> ${dns_config}
+        grep '192.168.29.230' ${dns_config} >/dev/null 2>&1 || echo -en 'nameserver 192.168.29.230\nnameserver 192.168.29.229\n' > ${dns_config}
 fi
 }
 
 set_yum_proxy () {
 yum_config='/etc/yum.conf'
 if [ -e ${yum_config} ];then
-	grep -E '^proxy*' ${yum_config} >/dev/null 2>&1 &&\
-	sed -r -i "s/^proxy.*$/proxy=http:\/\/${proxy_server}:${proxy_port}\//" ${yum_config} ||\
-	echo "proxy=http://${proxy_server}:${proxy_port}/" >> ${yum_config}
+        grep -E '^proxy*' ${yum_config} >/dev/null 2>&1 &&\
+        sed -r -i "s/^proxy.*$/proxy=http:\/\/${proxy_server}:${proxy_port}\//" ${yum_config} ||\
+        echo "proxy=http://${proxy_server}:${proxy_port}/" >> ${yum_config}
 fi
 }
 
@@ -95,15 +107,15 @@ yum clean all
 
 main () {
 yum_server='yum.suixingpay.local'
-proxy_server='squid.proxy.local'
-proxy_port='3128'
+#proxy_server='squid.proxy.local'
+#proxy_port='3128'
 check_system
 #check_repo_config
 check_platform
 backup_local_repo_file
 set_repo_file
 alias_yum
-set_dns_server
+#set_dns_server
 #set_yum_proxy
 clean_yum_cache
 echo "Done."
