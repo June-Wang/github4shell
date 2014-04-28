c5a327c934be09d4e4e1fd899143b9b88cf7a401
wang xiaojun <wang_xj@suixingpay.com>
2014-04-28 17:11:55 +0800

    重构安装nginx脚本

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,127 +1,37 @@
 #!/bin/bash
 
-init_var () {
-#set yum server
-yum_server="$1"
-file_name="$2"
-yum_para="$3"
-if [ "${yum_para}" = 'lan' ];then
-	YUM='yum --disablerepo=\* --enablerepo=centos5-lan'
-else
-	YUM='yum'
-fi
-package_url="http://${yum_server}/tools/${file_name}"
+#SET ENV
+YUM_SERVER='yum.suixingpay.local'
+PACKAGE_URL="http://${YUM_SERVER}/tools"
+
+#SET TEMP PATH
+TEMP_PATH='/usr/local/src'
+
+#SET TEMP DIR
+INSTALL_DIR="install_$$"
+INSTALL_PATH="${TEMP_PATH}/${INSTALL_DIR}"
+
+#SET PACKAGE
+YUM_PACKAGE='gcc gcc-c++ openssl openssl-devel glib2-devel pcre-devel bzip2-devel gzip-devel'
+APT_PACKAGE='build-essential'
+
+#SET EXIT STATUS AND COMMAND
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap "rm -rf ${INSTALL_PATH}"  EXIT
+
+download_func () {
+local func_shell='func4install.sh'
+local func_url="http://${YUM_SERVER}/shell/${func_shell}"
+local tmp_file="/tmp/${func_shell}"
+
+wget -q ${func_url} -O ${tmp_file} && source ${tmp_file} ||\
+eval "echo Can not access ${func_url}! 1>&2;exit 1"
+rm -f ${tmp_file}
 }
 
-create_nginx_user () {
-	grep 'nginx' /etc/passwd >/dev/null 2>&1 && userdel nginx
-	useradd -M -c "Nginx user" -s /bin/false -r -d /usr/local/nginx nginx
-}
-
-install_lib () {
-log_name="$1"
-log_file="${local_path}/yum_for_${log_name}.log"
-echo -n "install gcc openssl glib2 pcre bzip gzip please wait ......"
-eval "${YUM} install -y gcc gcc-c++ openssl openssl-devel glib2-devel pcre-devel bzip2-devel gzip-devel >${log_file} 2>&1" || yum_install='fail'
-if [ "${yum_install}" = "fail" ];then
-        echo "yum not available!view error please type: less ${log_file}" 1>&2
-        exit 1
-fi
-echo "done."
-}
-
-make_dir () {
-mkdir -p "${local_path}/${install_dir}" && cd "${local_path}/${install_dir}" || mkdir_dir='fail'
-if [ "${mkdir_dir}" = "fail"  ];then
-        echo "mkdir ${install_dir} fail!" 1>&2
-        exit 1
-fi
-}
-
-del_tmp () {
-#del tmp file
-test -d "${local_path}/${install_dir}" && rm -rf "${local_path}/${install_dir}"
-}
-
-check_urls () {
-for url in "$@"
-do
-        file=`echo ${url}|awk -F'/' '{print $NF}'`
-        if [ ! -f "${file}" ]; then
-                echo -n "download ${url} ..."
-                wget -q "${url}"  && echo 'done.' || download='fail'
-                if [ "${download}" = "fail" ];then
-                        echo "download ${url} fail!" 1>&2 && del_tmp
-                        exit 1
-                fi
-        fi
-done
-}
-
-install_pre () {
-        install_url="$1"
-        file=`echo ${install_url}|awk -F'/' '{print $NF}'`
-        dir=`echo ${file}|awk -F'.tar' '{print $1}'`
-        test -e "${file}" && tar xzf ${file} || tar_file='not_exist'
-        cd ${dir} || file_dir='not_exist'
-        if [ "${tar_file}" = 'not_exist' ];then
-                echo "${file} not exist!" 1>&2 && del_tmp
-                exit 1
-        fi
-        if [ "${file_dir}" = 'not_exist' ];then
-                echo "plesse check ${file}!" 1>&2 && del_tmp
-                exit 1
-        fi
-        echo -n "Compile ${dir} please wait ...... "
-}
-
-run_cmds () {
-        cmd_log="${local_path}/install_${dir}.log"
-        test -f "${cmd_log}" && cat /dev/null > "${local_path}/install_${dir}.log"
-        for cmd in "$@"
-        do
-                ${cmd} >> "${cmd_log}" 2>&1 || compile='fail'
-                if [ "${compile}" = 'fail' ]; then
-                        echo "run ${cmd} error! please type: less ${cmd_log}" 1>&2 && del_tmp
-                        exit 1
-                fi
-        done
-        echo "done."
-}
-
-install_nginx () {
-	install_pre "${package_url}"
-	./configure --user=nginx \
-        --group=nginx \
-        --prefix=/usr/local/nginx \
-        --sbin-path=/usr/sbin/nginx \
-        --conf-path=/etc/nginx/nginx.conf \
-        --error-log-path=/var/log/nginx/error.log \
-        --http-log-path=/var/log/nginx/access.log \
-        --pid-path=/var/run/nginx.pid \
-        --lock-path=/var/lock/subsys \
-        --with-http_ssl_module \
-        --with-http_realip_module \
-        --with-http_addition_module \
-        --with-http_sub_module \
-        --with-http_dav_module \
-        --with-http_flv_module \
-        --with-http_gzip_static_module \
-        --with-http_stub_status_module \
-        --with-http_perl_module \
-        --with-mail \
-        --with-mail_ssl_module >> ${local_path}/install_${dir}.log 2>&1 || configure='fail'
-	if [ "${configure}" = 'fail' ]; then
-                        echo "run ./configure error! please type: less ${local_path}/install_${dir}.log" 1>&2 && del_tmp
-                        exit 1
-                fi
-	run_cmds 'make' 'make install'
-	#test -d /var/lock/subsys/nginx || mkdir -p /var/lock/subsys/nginx
-	cd ..
-}
-
+##### FOR NGINX #####
 set_auto_run () {
-	echo '#!/bin/bash
+        echo '#!/bin/bash
 #
 # nginx - this script starts and stops the nginx daemin
 #
@@ -233,7 +143,7 @@ esac
 
 chmod +x /etc/rc.d/init.d/nginx
 
-auto_service="$1"
+auto_service='nginx'
 chkconfig --add "${auto_service}"
 chkconfig "${auto_service}" on
 }
@@ -268,7 +178,7 @@ done
 kill -USR1 `cat ${nginx_pid}` && exit 0' >/etc/nginx/nginx_logrotate.sh
 chmod +x /etc/nginx/nginx_logrotate.sh
 echo '#SET nginx logrotate _BEGIN_
-0 7 * * * root /etc/nginx/nginx_logrotate.sh >/dev/null
+0 0 * * * root /etc/nginx/nginx_logrotate.sh >/dev/null
 #SET nginx logrotate _END_' >>/etc/crontab
 fi
 }
@@ -286,37 +196,55 @@ mkdir -p ${backup_path} && cd ${backup_path}
 tar czf nginx-conf.${day}.tar.gz /etc/nginx/*
 
 test -f "nginx-conf.${rm_day}.tar.gz" && rm -f "nginx-conf.${rm_day}.tar.gz"
-' >/etc/nginx/backup_nginx_conf.sh
+' > /etc/nginx/backup_nginx_conf.sh
 chmod +x /etc/nginx/backup_nginx_conf.sh
 echo '#SET backup nginx conf  _BEGIN_
-0 0 * * * root /etc/nginx/backup_nginx_conf.sh >/dev/null
+0 7 * * * root /etc/nginx/backup_nginx_conf.sh >/dev/null
 #SET back nginx conf _END_' >>/etc/crontab
 fi
 }
 
-echo_bye () {
-	program="$1"
-	echo "Install ${program} complete! Please type : /etc/init.d/nginx start " && exit 0
-}
-
 main () {
-my_project='nginx'
-init_var 'yum.suixingpay.com' 'nginx-1.2.1.tar.gz' 'lan'
-install_lib "${my_project}"
-make_dir
-check_urls "${package_url}"
-create_nginx_user
-install_nginx
-set_auto_run "${my_project}"
+#DOWNLOAD FUNC FOR INSTALL
+download_func
+
+#CHECK SYSTEM AND CREATE TEMP DIR
+check_system
+#create_tmp_dir
+set_install_cmd 'lan'
+
+#CREATE nginx user
+NGINX_USER='www'
+create_user "${NGINX_USER}" "nologin"
+
+#Install nginx
+PACKAGE='nginx-1.6.0.tar.gz'
+create_tmp_dir
+download_and_check
+run_cmds "./configure --user=${NGINX_USER} \
+        --group=${NGINX_USER} \
+        --prefix=/usr/local/nginx \
+		--sbin-path=/usr/sbin \
+        --conf-path=/etc/nginx/nginx.conf \
+        --error-log-path=/var/log/nginx/error.log \
+        --http-log-path=/var/log/nginx/access.log \
+        --with-http_ssl_module \
+        --with-http_realip_module \
+        --with-http_addition_module \
+        --with-http_sub_module \
+        --with-http_dav_module \
+        --with-http_flv_module \
+        --with-http_gzip_static_module \
+        --with-http_stub_status_module \
+        --with-http_perl_module" 'make' 'make install'
+
+set_auto_run
 set_logrotate
-backup_nginx_conf
-del_tmp
-echo_bye "${my_project}"
+#backup_nginx_conf
+
+#EXIT AND CLEAR TEMP DIR
+exit_and_clear
+
 }
 
-#local install path
-local_path='/usr/local/src'
-install_dir="install_$$"
-trap "exit 1"           HUP INT PIPE QUIT TERM
-trap "rm -f ${install_dir}"  EXIT
 main
