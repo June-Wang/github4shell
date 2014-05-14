756610385709208c2065b0fc91422de522c3ce95
wang xiaojun <wang_xj@suixingpay.com>
2014-05-14 10:51:31 +0800

    重命名

--- install_denyhosts.sh
+++ install_denyhosts.sh
@@ -1,114 +1,32 @@
 #!/bin/bash
 
-check_system (){
-system_info=`head -n 1 /etc/issue`
-case "${system_info}" in
-        'CentOS release 5'*)
-                SYSTEM='centos5'
-                YUM_SOURCE_NAME='centos5-lan'
-                ;;
-        'Red Hat Enterprise Linux Server release 5'*)
-                SYSTEM='rhel5'
-                YUM_SOURCE_NAME='RHEL5-lan'
-                ;;
-        *)
-                SYSTEM='unknown'
-                echo "This script not support ${system_info}" 1>&2
-                exit 1
-                ;;
-esac
-}
+#SET ENV
+YUM_SERVER='yum.suixingpay.local'
+PACKAGE_URL="http://${YUM_SERVER}/tools"
 
-alias_yum () {
-local yum_para="$1"
+#SET TEMP PATH
+TEMP_PATH='/usr/local/src'
 
-check_system
-if [ "${yum_para}" = 'lan' ];then
-        YUM="yum --disablerepo=\* --enablerepo=${yum_source_name}"
-else
-        YUM='yum'
-fi
-}
+#SET TEMP DIR
+INSTALL_DIR="install_$$"
+INSTALL_PATH="${TEMP_PATH}/${INSTALL_DIR}"
 
-create_user () {
-        username="$1"
-        grep "${username}" /etc/passwd >/dev/null 2>&1 || useradd  -c "${username} user" -s /sbin/nologin ${username}
-}
+#SET PACKAGE
+#YUM_PACKAGE='gcc glibc glibc-common make cmake gcc-c++'
+#APT_PACKAGE='build-essential'
 
-install_lib () {
-local log_file="${TEMP_PATH}/yum_for_${MY_PROJECT}.log"
+#SET EXIT STATUS AND COMMAND
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap "rm -rf ${INSTALL_PATH}"  EXIT
 
-echo -n "install gcc gcc-c++ make please wait ...... "
-eval "${YUM} install -y gcc gcc-c++ make >${log_file} 2>&1" || local yum_install='fail'
-if [ "${yum_install}" = "fail" ];then
-        echo -e "yum not available!\nview error please type: less ${log_file}" 1>&2
-        exit 1
-fi
-echo "done."
-}
+download_func () {
+local func_shell='func4install.sh'
+local func_url="http://${YUM_SERVER}/shell/${func_shell}"
+local tmp_file="/tmp/${func_shell}"
 
-make_tmp_dir () {
-mkdir -p "${INSTALL_PATH}" && cd "${INSTALL_PATH}" || local mkdir_dir='fail'
-if [ "${mkdir_dir}" = "fail"  ];then
-        echo "mkdir ${INSTALL_PATH} fail!" 1>&2
-        exit 1
-fi
-}
-
-del_tmp () {
-test -d "${INSTALL_PATH}" && rm -rf "${INSTALL_PATH}"
-}
-
-download_file () {
-local   url="$1"
-local   file=`echo ${url}|awk -F'/' '{print $NF}'`
-
-if [ ! -f "${file}" ]; then
-        echo -n "download ${url} ...... "
-        wget -q "${url}"  && echo 'done.' || local download='fail'
-        if [ "${download}" = "fail" ];then
-                echo "download ${url} fail!" 1>&2 && del_tmp
-                exit 1
-        fi
-fi
-}
-
-check_file () {
-local file="$1"
-local ex_dir=`echo "${file}"|awk -F'.tar|.tgz' '{print $1}'`
-local dir="${INSTALL_PATH}/${ex_dir}"
-
-test -f ${file} && tar xzf ${file} || eval "echo ${file} not exsit!;del_tmp;exit 1"
-test -d ${dir} && cd ${dir} || eval "echo ${dir} not exsit!;del_tmp;exit 1"
-echo -n "Compile ${file} please wait ...... "
-}
-
-run_cmds () {
-local   cmd_log="${TEMP_PATH}/install_${PACKAGE}.log"
-        test -f "${cmd_log}" && cat /dev/null > "${TEMP_PATH}/install_${dir}.log"
-        for cmd in "$@"
-        do
-                ${cmd} >> "${cmd_log}" 2>&1 || compile='fail'
-                if [ "${compile}" = 'fail' ]; then
-                        echo "run ${cmd} error! please type: less ${cmd_log}" 1>&2 && del_tmp
-                        exit 1
-                fi
-        done
-        echo "done."
-}
-
-install_denyhosts () {
-        download_file "${PACKAGE_URL}"
-        check_file "${PACKAGE}"
-        run_cmds 'python setup.py install'
-        denyhosts_init='/usr/share/denyhosts/daemon-control-dist'
-        start_file='/etc/init.d/denyhosts'
-        test -e ${start_file} && rm -f ${start_file}
-        test -e ${denyhosts_init} && ln -s ${denyhosts_init} ${start_file}
-        denyhosts_cmd='/usr/bin/denyhosts.py'
-        test -e ${denyhosts_cmd} || ln -s /usr/local/bin/denyhosts.py ${denyhosts_cmd}
-        test -d /var/lock/subsys/ || mkdir -p /var/lock/subsys/
-        cd ..
+wget -q ${func_url} -O ${tmp_file} && source ${tmp_file} ||\
+eval "echo Can not access ${func_url}! 1>&2;exit 1"
+rm -f ${tmp_file}
 }
 
 config_denyhosts () {
@@ -138,18 +56,10 @@ AGE_RESET_RESTRICTED=25d
 AGE_RESET_INVALID=10d
 DAEMON_LOG = /var/log/denyhosts
 DAEMON_LOG_TIME_FORMAT = %F %T
+local MY_PROJECT='denyhosts'
 DAEMON_SLEEP = 30s
 DAEMON_PURGE = 1h' > ${denyhosts_config}
         /etc/init.d/denyhosts restart
-}
-
-set_auto_run () {
-        chkconfig --add ${MY_PROJECT}
-        chkconfig ${MY_PROJECT} on
-}
-
-echo_bye () {
-        echo "Install ${PACKAGE} complete!" && exit 0
 }
 
 add_nagios () {
@@ -169,26 +79,37 @@ fi
 }
 
 main () {
-INSTALL_PATH="${TEMP_PATH}/${INSTALL_DIR}"
-PACKAGE_URL="http://${YUM_SERVER}/tools/${PACKAGE}"
-make_tmp_dir
-install_${MY_PROJECT}
-config_${MY_PROJECT}
-set_auto_run
-del_tmp
+#DOWNLOAD FUNC FOR INSTALL
+download_func
+
+#CHECK SYSTEM AND CREATE TEMP DIR
+check_system
+#create_tmp_dir
+set_install_cmd 'lan'
+
+#Install DenyHosts-2.6
+PACKAGE='DenyHosts-2.6.tar.gz'
+create_tmp_dir
+download_and_check
+run_cmds 'python setup.py install'
+denyhosts_init='/usr/share/denyhosts/daemon-control-dist'
+init_file='/etc/init.d/denyhosts'
+test -e ${init_file} && rm -f ${init_file}
+test -e ${denyhosts_init} && ln -s ${denyhosts_init} ${init_file}
+denyhosts_cmd='/usr/bin/denyhosts.py'
+test -e ${denyhosts_cmd} || ln -s /usr/local/bin/denyhosts.py ${denyhosts_cmd}
+test -d /var/lock/subsys/ || mkdir -p /var/lock/subsys/
+#Setting
+config_denyhosts
+#Add2nagios
 add_nagios
-echo_bye
+#set auto run
+MY_PROJECT='denyhosts'
+set_auto_run "${MY_PROJECT}"
+
+#EXIT AND CLEAR TEMP DIR
+exit_and_clear
+
 }
 
-#SET TEMP PATH
-TEMP_PATH='/usr/local/src'
-INSTALL_DIR="install_$$"
-
-#SET GLOBAL VAR
-MY_PROJECT='denyhosts'
-PACKAGE='DenyHosts-2.6.tar.gz'
-YUM_SERVER='yum.suixingpay.com'
-
-trap "exit 1"           HUP INT PIPE QUIT TERM
-trap "rm -rf ${INSTALL_PATH}"  EXIT
 main
