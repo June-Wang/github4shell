598ec644cadcac2e881b9861fc767abfed4dc1cc
Alex wang <qq5910225@gmail.com>
2014-08-06 11:15:32 +0800

    添加安装mk客户端脚本

---
+++ install_mk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+nagios_server='192.168.16.21'
+yum_server='yum.lefu.local'
+
+apt_cmd='aptitude install -y'
+echo -en "${apt_cmd} xinetd ..."
+eval "${apt_cmd} xinetd" >/dev/null 2>&1 || eval "echo ${apt_cmd} xinetd error!;exit 1" &&\
+echo "done."
+
+package='check-mk-agent_1.2.4p5-2_all.deb'
+echo  "Install ${package} ..."
+eval "wget http://${yum_server}/tools/${package} -O /tmp/${package} >/dev/null" && dpkg -i /tmp/${package}
+
+check_mk_conf='/etc/xinetd.d/check_mk'
+test -f ${check_mk_conf} || eval "echo check_mk not been installed!;exit 1" &&\
+sed -r -i "s/#only_from.*/only_from = 127.0.0.1 ${nagios_server}/" ${check_mk_conf} 
+
+/etc/init.d/xinetd restart
