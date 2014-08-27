bf46c63acf72b950591c3b9bc07f906159a65e7d
Alex wang <qq5910225@gmail.com>
2014-08-27 17:48:35 +0800

    新增修改配置

--- install_mod_gearman.sh
+++ install_mod_gearman.sh
@@ -60,6 +60,31 @@ done
 download_and_check
 run_cmds './configure --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc/ --libdir=/usr/lib' 'make' 'make install' 'make install-config'
 
+#nagios setting
+nagios_conf='/usr/local/nagios/etc/nagios.cfg'
+if [ -f ${nagios_conf} ];then
+	mod_gearman_neb_conf='/etc/mod_gearman/mod_gearman_neb.conf'
+	grep '#Add Mod_Gearman' ${nagios_conf} >/dev/null 2>&1 ||\
+	echo "#Add Mod_Gearman
+	broker_module=/usr/lib/mod_gearman/mod_gearman.o config=${mod_gearman_neb_conf}" >> ${nagios_conf}
+	test -f ${mod_gearman_neb_conf} &&\
+	sed -i -r 's/^encryption=.*/encryption=no/;s/^key=/#key=/;s/^server=.*/server=127.0.0.1:4730/;s|logfile=.*|logfile=/var/log/mod_gearman/mod_gearman_neb.log|' ${mod_gearman_neb_conf}
+	log_path='/var/log/mod_gearman/'
+	mkdir -p ${log_path} && chown nagios -R ${log_path}
+	test -f /etc/init.d/nagios && /etc/init.d/nagios restart
+fi
+
+#mod gearman worker
+
+mod_gearman_worker_conf='/etc/mod_gearman/mod_gearman_worker.conf'
+test -f ${mod_gearman_worker_conf} &&\
+sed -i -r 's/^encryption=.*/encryption=no/;s/^key=/#key=/;s/^server=.*/server=127.0.0.1:4730/;s/^perfdata=.*/perfdata=yes/;s|logfile=.*|logfile=/var/log/mod_gearman/mod_gearman_worker.log|' ${mod_gearman_worker_conf} 
+test -f /etc/init.d/mod_gearman_worker && sed -r -i 's|LOCKFILE=.*|LOCKFILE=/tmp/\$NAME|' /etc/init.d/mod_gearman_worker
+/etc/init.d/mod_gearman_worker restart
+grep '#mod gearman' >/dev/null 2>&1 /etc/rc.local ||\
+echo "/usr/local/sbin/gearmand -t 10 -j 0 -d" >> /etc/rc.local
+/usr/local/sbin/gearmand -t 10 -j 0 -d
+
 #EXIT AND CLEAR TEMP DIR
 exit_and_clear
 
