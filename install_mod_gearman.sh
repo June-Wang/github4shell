9a68564a8eb5a2b7c067961721f0167df93dd5f8
Alex wang <qq5910225@gmail.com>
2014-08-28 08:31:32 +0800

    添加mod_gearman_worker启动项

--- install_mod_gearman.sh
+++ install_mod_gearman.sh
@@ -13,7 +13,7 @@ INSTALL_PATH="${TEMP_PATH}/${INSTALL_DIR}"
 
 #SET PACKAGE
 YUM_PACKAGE='gcc glibc glibc-common make cmake gcc-c++ ncurses-devel'
-APT_PACKAGE='libevent-dev uuid-dev libncurses-dev autoconf automake libtool libboost-all-dev gperf'
+APT_PACKAGE='libevent-dev uuid-dev libncurses-dev autoconf automake libtool libboost-all-dev gperf chkconfig'
 
 #SET EXIT STATUS AND COMMAND
 trap "exit 1"           HUP INT PIPE QUIT TERM
@@ -66,7 +66,7 @@ if [ -f ${nagios_conf} ];then
 	mod_gearman_neb_conf='/etc/mod_gearman/mod_gearman_neb.conf'
 	grep '#Add Mod_Gearman' ${nagios_conf} >/dev/null 2>&1 ||\
 	echo "#Add Mod_Gearman
-	broker_module=/usr/lib/mod_gearman/mod_gearman.o config=${mod_gearman_neb_conf}" >> ${nagios_conf}
+broker_module=/usr/lib/mod_gearman/mod_gearman.o config=${mod_gearman_neb_conf}" >> ${nagios_conf}
 	test -f ${mod_gearman_neb_conf} &&\
 	sed -i -r 's/^encryption=.*/encryption=no/;s/^key=/#key=/;s/^server=.*/server=127.0.0.1:4730/;s|logfile=.*|logfile=/var/log/mod_gearman/mod_gearman_neb.log|' ${mod_gearman_neb_conf}
 	log_path='/var/log/mod_gearman/'
@@ -75,12 +75,13 @@ if [ -f ${nagios_conf} ];then
 fi
 
 #mod gearman worker
-
 mod_gearman_worker_conf='/etc/mod_gearman/mod_gearman_worker.conf'
 test -f ${mod_gearman_worker_conf} &&\
 sed -i -r 's/^encryption=.*/encryption=no/;s/^key=/#key=/;s/^server=.*/server=127.0.0.1:4730/;s/^perfdata=.*/perfdata=yes/;s|logfile=.*|logfile=/var/log/mod_gearman/mod_gearman_worker.log|' ${mod_gearman_worker_conf} 
 test -f /etc/init.d/mod_gearman_worker && sed -r -i 's|LOCKFILE=.*|LOCKFILE=/tmp/\$NAME|' /etc/init.d/mod_gearman_worker
 /etc/init.d/mod_gearman_worker restart
+chkconfig mod_gearman_worker on
+
 grep '#mod gearman' >/dev/null 2>&1 /etc/rc.local ||\
 echo "#mod gearman
 /usr/local/sbin/gearmand -t 10 -j 0 -d" >> /etc/rc.local
