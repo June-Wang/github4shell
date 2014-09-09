11e931bef966776de83481c588170754acb953c8
Alex wang <qq5910225@gmail.com>
2014-09-09 08:50:46 +0800

    添加ulimited设置

--- init_debian.sh
+++ init_debian.sh
@@ -66,6 +66,23 @@ else
 fi
 echo 'done.'
 
+#set ulimit
+grep -E '^ulimit.*' /etc/rc.local >/dev/null 2>&1 || echo "ulimit -Sn 4096
+ulimit -Hn 10240" >> /etc/rc.local
+limit_conf='/etc/security/limits.conf'
+grep -E '^#-=SET Ulimit=-' ${limit_conf} >/dev/null 2>&1 ||set_limit="no"
+if [ "${set_limit}" = 'no' ];then
+test -f ${limit_conf} && echo '
+#-=SET Ulimit=-
+* soft nofile 4096
+* hard nofile 10240
+' >> ${limit_conf}
+fi
+
+if [ -f /etc/pam.d/su ];then
+        sed -r -i 's|^.*pam_limits.so.*$|session    required   pam_limits.so|g' /etc/pam.d/su
+fi
+
 #add sysinfo
 grep 'cron_sysinfo.sh' /etc/crontab >/dev/null 2>&1 || cron_sysinfo_set='no'
 if [ "${cron_sysinfo_set}" = "no" ];then
