d4a41f7355a70dcae8a32ab373b08a57880a293d
Alex wang <qq5910225@gmail.com>
2014-09-23 14:17:02 +0800

    添加服务端口自动到checkMK

---
+++ add_serv2mrpe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#test path
+test -d /etc/check_mk/ || mkdir -p /etc/check_mk/
+
+#test file
+mrpe_file='/etc/check_mk/mrpe.cfg'
+test -f ${mrpe_file} || touch ${mrpe_file}
+
+#add serv
+netstat -lnp|awk '$(NF-1)~/LISTEN/ && $NF~/java/{print $4}'|\
+grep -v '127.0.0.1'|grep -oP '\d+$'|\
+while read serv_port
+do
+        grep "check_tcp_${serv_port}" ${mrpe_file} >/dev/null 2>&1 ||\
+        echo "check_tcp_${serv_port} /usr/local/nagios/libexec/check_tcp -w 2 -c 3 -t 4 -p ${serv_port}" >> ${mrpe_file} &&\
+        echo "add ${serv_port} to mrpe!"
+done
