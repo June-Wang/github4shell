534c70fb77f385417901c01890553b8812ef44ad
Alex wang <qq5910225@gmail.com>
2014-09-30 11:45:54 +0800

    添加检测TOMCAT服务端口超时自动重启服务脚本

---
+++ check_tomcat_serv.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+help () {
+        local command=`basename $0`
+        echo "/bin/bash ${command} -s <service name> -p <port>
+/bin/bash ${command} -s tomcat_agent -p 8080" 1>&2
+        exit 1
+}
+
+while getopts s:p: opt
+do
+        case "$opt" in
+        s)
+                serv_name=$OPTARG
+                app_path="/opt/${serv_name}/bin"
+                test -d ${app_path} ||\
+                eval "echo ${app_path} not found!;exit 1"
+                ;;
+        p)
+                serv_port=$OPTARG
+                netstat -nlt 2>/dev/null|grep -E ":${serv_port} " >/dev/null 2>&1 ||\
+                eval "echo port: ${serv_port} not listening!please check!;exit 1"
+                ;;
+        *) help;;
+        esac
+done
+shift $[ $OPTIND - 1 ]
+
+[ $# -gt 0 -o -z "${serv_name}" -o -z "${serv_port=}" ] && help
+
+pid=`echo $$`
+file_name=`basename $0`
+lock_file="/tmp/${file_name}.$$"
+
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap "rm -f ${lock_file}"  EXIT
+
+test -f ${lock_file} && stat='run'
+
+if [ "${stat}" = 'run' ];then
+        echo "$0 (pid  `cat ${lock_file}`) is running..." 1>&2
+        exit 1
+else
+        echo "${pid}" > ${lock_file}
+fi
+
+serv_pid=`pgrep -u deployer java|\
+while read pid
+do 
+        ps -eo pid,args|\
+        grep "$pid"|\
+        grep "${serv_name}"|\
+        awk '{print $1}'
+done`
+
+#debug
+#echo "${app_path}"
+
+for i in `seq 3`
+do
+        curl -Is --connect-timeout 5 -m 10 http://127.0.0.1:${serv_port} >/dev/null &&\
+        eval "echo ${serv_name} looks OK!;exit 0"
+        sleep 2
+done
+
+user_name=`whoami`
+profile="/home/${user_name}/.profile"
+test -f ${profile} && source ${profile}
+
+kill -9 ${serv_pid}
+cd ${app_path} && ./startup.sh && sleep 5s
