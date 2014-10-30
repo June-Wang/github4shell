c2a4073aa5f64c5a0943203a19b5f7a6103e7506
Alex wang <qq5910225@gmail.com>
2014-10-30 15:34:33 +0800

    修改判断语句

--- add_serv2mrpe.sh
+++ add_serv2mrpe.sh
@@ -12,7 +12,11 @@ netstat -lnp|awk '$(NF-1)~/LISTEN/ && $NF~/java/{print $4}'|\
 grep -v '127.0.0.1'|grep -oP '\d+$'|\
 while read serv_port
 do
-        grep "check_tcp_${serv_port}" ${mrpe_file} >/dev/null 2>&1 ||\
-        echo "check_tcp_${serv_port} /usr/local/nagios/libexec/check_tcp -w 2 -c 3 -t 4 -p ${serv_port}" >> ${mrpe_file} &&\
-        echo "add ${serv_port} to mrpe!"
+        grep "check_tcp_${serv_port}" ${mrpe_file} >/dev/null 2>&1 ||stat='0'
+        if [ "${stat}" == '0' ];then
+                echo "check_tcp_${serv_port} /usr/local/nagios/libexec/check_tcp -w 2 -c 3 -t 4 -p ${serv_port}" >> ${mrpe_file}
+                echo "add ${serv_port} to mrpe!"
+        else
+                echo "${serv_port} has been added to mrpe!"
+        fi
 done
