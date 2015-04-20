206af89a0886c564efae08b91eb74d523b18785b
Alex Wang <qq5910225@gmail.com>
2015-04-21 00:55:47 +0800

    修改路径

--- modify_mrpe.sh
+++ modify_mrpe.sh
@@ -6,10 +6,10 @@ netstat -ntlp 2>/dev/null|grep 'java'|grep -v '127.0.0.1'|awk '{print $4,$NF}'|g
 while read port pid
 do
         echo -en "$port\t"
-        ps -eo pid,args|grep "$pid"|grep -oP '/opt/.[^/| ]*/'|grep -Ev 'JDK|grep'|sort -u
+        ps -eo pid,args|grep "$pid"|grep -oP '/home/deployer/.[^/| ]*/'|grep -Ev 'JDK|grep'|sort -u
 done|sort -u|sort -k2|\
 while read port path
 do
-        serv_name=`echo $path|awk -F'/' '{print $3}'`
+        serv_name=`echo $path|awk -F'/' '{print $4}'`
         echo "check_tcp_${serv_name}_${port} /usr/local/nagios/libexec/check_tcp -w 2 -c 3 -t 4 -p ${port}"
 done >> ${mrpe_conf}
