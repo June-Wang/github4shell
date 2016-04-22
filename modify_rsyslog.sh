aef86962deac672f8847779342dd1723c297b250
Alex Wang <qq5910225@gmail.com>
2016-04-23 02:21:29 +0800

    rsyslog格式化

--- modify_rsyslog.sh
+++ modify_rsyslog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum_server='10.54.1.110'
+yum_server='yum.server.local'
 rsyslogd_path='/etc/rsyslog.d'
 test -f ${rsyslogd_path}/rsyslog.format.conf ||\
 wget http://${yum_server}/shell/rsyslog.format.conf -O ${rsyslogd_path}/rsyslog.format.conf
