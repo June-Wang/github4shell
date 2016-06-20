d2d8756fc09e4c30f04aa2ae9cec149936316844
Alex Wang <qq5910225@gmail.com>
2016-06-20 11:06:03 +0800

    修改

--- get_ip.sh
+++ get_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ip address show|grep inet|awk '{print $NF"\t"$2}'|grep -oP '^.+\d{1,3}(\.\d{1,3}){3}'
+ip address show|awk '/inet/{print $NF"\t"$2}'|grep -oP '^.+\d{1,3}(\.\d{1,3}){3}'
