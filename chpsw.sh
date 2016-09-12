4a8f5c80c824450afe046160c6ea5ebab3ca3590
Alex Wang <qq5910225@gmail.com>
2016-09-12 12:46:42 +0800

    同时修改多个账户

--- chpsw.sh
+++ chpsw.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-USER='root'
+ip=`/sbin/ip address show|awk '/inet/{print $2}'|grep -oP '^.+\d{1,3}(\.\d{1,3}){3}'|grep 10.211|head -n1`
 
-passwd=`openssl rand -base64 11|grep -oE '^.{10}'|head -n 1`
+output_file="/tmp/pswd.file.$$"
 
-ip=`ip address show|awk '/inet/{print $2}'|grep -oP '^.+\d{1,3}(\.\d{1,3}){3}'|grep -Ev '127'|head -n1`
+#SET EXIT STATUS AND COMMAND
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap "test -f ${output_file} && rm -f ${output_file}"  EXIT
 
-echo ${USER}:${passwd}|chpasswd
+grep 'bash' /etc/passwd |awk -F':' '{print $1}'|\
+while read user_id
+do
+        passwd=`openssl rand -base64 11|grep -oE '^.{15}'|head -n 1`
+        #echo ${user_id}:${passwd}|/usr/sbin/chpasswd
+        echo -en "${ip} ${user_id} ${passwd}\n" >> ${output_file}
+done
 
-echo -en "${ip}\t${USER}\t${passwd}\n"
+test -f ${output_file} && cat ${output_file}
