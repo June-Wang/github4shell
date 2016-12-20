6d48d924884c13b514954f71a01932bcbd882e83
Alex Wang <qq5910225@gmail.com>
2016-12-20 14:51:50 +0800

    修改

--- chpsw.sh
+++ chpsw.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-ip=`/sbin/ip address show|awk '/inet/{print $2}'|grep -oP '^.+\d{1,3}(\.\d{1,3}){3}'|grep 10.211|head -n1`
+USER="$1"
 
-output_file="/tmp/pswd.file.$$"
+passwd=`openssl rand -base64 11|grep -oE '^.{10}'|head -n 1`
 
-#SET EXIT STATUS AND COMMAND
-trap "exit 1"           HUP INT PIPE QUIT TERM
-trap "test -f ${output_file} && rm -f ${output_file}"  EXIT
+ip=`ip address show|grep -oP '\d{1,3}(\.\d{1,3}){3}'|grep -Ev '^127|255$|\.0$'|head -n1`
 
-grep 'bash' /etc/passwd |awk -F':' '{print $1}'|\
-while read user_id
-do
-        passwd=`openssl rand -base64 11|grep -oE '^.{15}'|head -n 1`
-        #echo ${user_id}:${passwd}|/usr/sbin/chpasswd
-        echo -en "${ip} ${user_id} ${passwd}\n" >> ${output_file}
-done
+echo ${USER}:${passwd}|chpasswd
 
-test -f ${output_file} && cat ${output_file}
+echo -en "${ip}\t${USER}\t${passwd}\n"
