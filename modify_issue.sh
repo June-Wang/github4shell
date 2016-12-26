8e77928cd989571021f74a41a29c065d112cc25a
Alex Wang <qq5910225@gmail.com>
2016-12-26 08:47:28 +0800

    修改

--- modify_issue.sh
+++ modify_issue.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-id=`ip address show|awk '/inet/{print $2}'|grep -oP '^.+\d{1,3}(\.\d{1,3}){3}'|grep -Ev '127'|head -n1`
+id=`ip address show|grep -oP '\d{1,3}(\.\d{1,3}){3}'|grep -Ev '^127|255$|\.0$'|head -n1`
 
 test -f /etc/issue && \
 echo -en "IP:\t${id}\n" > /etc/issue
