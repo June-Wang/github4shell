528eab1d848438b1feee14afbdd276703c69e4df
Alex Wang <qq5910225@gmail.com>
2017-04-20 14:38:37 +0800

    修改

--- modify_hostname.sh
+++ modify_hostname.sh
@@ -4,11 +4,11 @@ ip=`ip address show|grep -oP '\d{1,3}(\.\d{1,3}){3}'|grep -Ev '^127|255$|\.0$'`
 
 host_name=`echo $ip|awk -F'.' '{print "HOST"$NF}'`
 
-#echo ${host_name}
+test -f /usr/bin/hostnamectl && /usr/bin/hostnamectl set-hostname ${host_name}
 
 test -f /etc/sysconfig/network && sed -r -i "s/^HOSTNAME.*/HOSTNAME=${host_name}/" /etc/sysconfig/network
 
-test -f /etc/hosts && sed -r -i "/^${ip}/d" /etc/hosts
+test -f /etc/hosts && sed -r -i "/^${ip} /d" /etc/hosts
 
 grep "${host_name}" /etc/hosts >/dev/null 2>&1 ||\
 echo "${ip} ${host_name}" >> /etc/hosts
