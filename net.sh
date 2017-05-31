dbea9e7fbf531fe994d3c15a7016631b66d177de
Alex Wang <qq5910225@gmail.com>
2017-05-31 14:34:18 +0800

    full

--- net.sh
+++ net.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
 ip="$1"
-net=`echo ${ip}|awk -F'.' '{print $3}'`
-host=`echo ${ip}|awk -F'.' '{print $4}'`
 
+test -f /etc/redhat-release ||\
+eval "echo Not suport this system!;exit 1"
+
+echo ${ip}|grep -oP '\d{1,3}(\.\d{1,3}){3}' >/dev/null 2>&1 ||\
+eval "echo ${ip} not ip address!;exit 1"
+
+net=`echo ${ip}|grep -oE '^.*\.'`
+now=`date -d now +"%F-%H-%M-%S"`
+version=`grep -oP '\d' /etc/redhat-release |head -n1`
+
+if [ "${version}" == '6' ];then
 test -f /etc/sysconfig/network-scripts/ifcfg-eth0 && \
-cp /etc/sysconfig/network-scripts/ifcfg-eth0 /tmp/ifcfg-eth0.$$
+cp /etc/sysconfig/network-scripts/ifcfg-eth0 /tmp/ifcfg-eth0.bak.${now}
 
-sed  "s/_NET_/${net}/g;s/_IP_/${ip}/g" /tmp/ifcfg-eth0 > /etc/sysconfig/network-scripts/ifcfg-eth0
+echo "DEVICE=eth0
+TYPE=Ethernet
+ONBOOT=yes
+NM_CONTROLLED=yes
+BOOTPROTO=static
+IPADDR=${ip}
+PREFIX=24
+GATEWAY=${net}254
+" > /etc/sysconfig/network-scripts/ifcfg-eth0
 
 sed -r -i "s/^HOSTNAME.*$/HOSTNAME=HOST${host}/" /etc/sysconfig/network
 sed -r -i "s/127.0.0.1   localhost/127.0.0.1  ${host} localhost/" /etc/hosts
-#echo "hostname ${hostname}" >> /etc/rc.local
 test -f /etc/udev/rules.d/70-persistent-net.rules &&\
 grep '^#' /etc/udev/rules.d/70-persistent-net.rules |sed '/PCI device/d' > /etc/udev/rules.d/70-persistent-net.rules
+fi
+
+if [ "${version}" == '7' ];then
+test -f /etc/sysconfig/network-scripts/ifcfg-eno16780032 &&\
+cp /etc/sysconfig/network-scripts/ifcfg-eno16780032 /etc/sysconfig/network-scripts/ifcfg-ens192 &&\
+mv /etc/sysconfig/network-scripts/ifcfg-eno16780032 /tmp/network-scripts/ifcfg-eno16780032.bak.${now}
+sed -i -r "s/eno16780032/ens192/g;s/^IPADDR=.*$/IPADDR=/${ip}/g;s/^GATEWAY=.*/GATEWAY=${net}254/g" /etc/sysconfig/network-scripts/ifcfg-ens192
+fi
 
 test -f /tmp/net.sh && rm -f /tmp/net.sh
 
