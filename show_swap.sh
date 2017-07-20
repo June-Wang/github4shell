221f363086b8bb60cc0595d14cf86147a61bfec5
Alex Wang <qq5910225@gmail.com>
2017-07-20 15:26:21 +0800

    重写

--- show_swap.sh
+++ show_swap.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-which bc >/dev/null ||\
-eval "echo bc not found!;exit 1" 
-
-user=`whoami`
-uid=`id ${user} -u`
-
-if [ "${uid}" != '0' ];then
-        echo "Sorry!You must be root to run this script!" 1>&2
-        exit 1
-fi
-
 human_read () {
 local number="$1"
 if [ `echo "(${number}-1073741824) > 0"|bc` -eq 1 ];then
@@ -25,22 +14,18 @@ fi
 echo "${output}"
 }
 
-echo -en "PID\tSwap\tProc_Name\n"
 ps -eo pid|sed '1d'|\
 while read pid
 do
-        smaps="/proc/${pid}/smaps"
-        test -f ${smaps} || continue
-        test -s ${smaps} && continue
-        pname=`ps -eo pid,args|awk -v id="$pid" '$1 == id{$1="";print}'|sed 's/^[ ]+//'`
-        swap=`awk '/Swap:/{sum+=$2}END{print sum}' ${smaps}`
+        test -f /proc/${pid}/smaps || continue
+        swap=`sudo awk '/^Swap:/ {SWAP+=$2}END{print SWAP}' /proc/${pid}/smaps`
         test -z "${swap}" && continue
-        if [ "$swap" != '0' -a ${pid} -gt 1 ];then
-                echo -en "$pid\t$swap\t$pname\n"
-        fi
-done|grep -E '^[1-9]'|sort -nrk2|\
-while read pid swap pname
+        [ ${swap} -gt 0 ] || continue
+        echo -en "$pid\t$swap\n"
+done|sort -nrk2|\
+while read pid swap
 do
+        pname=`ps -eo pid,args|awk -v id="$pid" '$1 == id{$1="";print}'|sed 's/^[ ]+//'|grep -oP '^.{1,140}'|head -n1`
         swap=`human_read "${swap}"`
         echo -en "${pid}\t${swap}\t${pname}\n"
-done
+done|sed '1i\pid\tswap\tcommand'
