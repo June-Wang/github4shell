d88bcec3894e05c0e69b8aaaa600fbd0e55c7bf8
Alex Wang <qq5910225@gmail.com>
2017-07-20 15:36:06 +0800

    修改

--- show_swap.sh
+++ show_swap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+uid=`id -u`
+[ ${uid} != 0 ] &&\
+eval "echo You must be root to run this script!;exit 1"
+
 human_read () {
 local number="$1"
 if [ `echo "(${number}-1073741824) > 0"|bc` -eq 1 ];then
@@ -18,7 +22,7 @@ ps -eo pid|sed '1d'|\
 while read pid
 do
         test -f /proc/${pid}/smaps || continue
-        swap=`sudo awk '/^Swap:/ {SWAP+=$2}END{print SWAP}' /proc/${pid}/smaps`
+        swap=`awk '/^Swap:/ {SWAP+=$2}END{print SWAP}' /proc/${pid}/smaps`
         test -z "${swap}" && continue
         [ ${swap} -gt 0 ] || continue
         echo -en "$pid\t$swap\n"
