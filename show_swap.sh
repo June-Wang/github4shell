45b8351107c44bad183d86ad141525f743495e64
Alex Wang <qq5910225@gmail.com>
2017-07-20 12:35:30 +0800

    改

--- show_swap.sh
+++ show_swap.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
+which bc >/dev/null ||\
+eval "echo bc not found!;exit 1" 
+
 user=`whoami`
 uid=`id ${user} -u`
 
 if [ "${uid}" != '0' ];then
-	echo "Sorry!You must be root to run this script!" 1>&2
-	exit 1
+        echo "Sorry!You must be root to run this script!" 1>&2
+        exit 1
 fi
 
-printf "%-5s\t%15s\t%20s\n" PID Swap Proc_Name
-find /proc/ -maxdepth 2 -name "smaps"|\
-while read smaps
+human_read () {
+local number="$1"
+if [ `echo "(${number}-1073741824) > 0"|bc` -eq 1 ];then
+        output="`echo "scale=2;${number}/1024/1024/1024"|bc`T"
+elif [ `echo "(${number}-1048576) > 0"|bc` -eq 1 ];then
+        output="`echo "scale=2;${number}/1024/1024"|bc`G"
+elif [ `echo "(${number}-1024) > 0"|bc` -eq 1 ];then
+        output="`echo "scale=2;${number}/1024"|bc`M"
+else
+        output="${number}K"
+fi
+echo "${output}"
+}
+
+echo -en "PID\tSwap\tProc_Name\n"
+ps -eo pid|sed '1d'|\
+while read pid
 do
+        smaps="/proc/${pid}/smaps"
         test -f ${smaps} || continue
-        pid=`echo ${smaps}|awk -F'/' '{pid=$3;print pid}'`
-        [ "$pid" = '1' ] && continue
-        pname=`ps -eo pid,args|grep "${pid}"|grep -v 'grep'|awk '{$1="";print}'|sed 's/^[ ]+//'`
+        test -s ${smaps} && continue
+        pname=`ps -eo pid,args|awk -v id="$pid" '$1 == id{$1="";print}'|sed 's/^[ ]+//'`
         swap=`awk '/Swap:/{sum+=$2}END{print sum}' ${smaps}`
-        [ "$swap" = '0' -o "$swap" = '' ] && continue
-        if [ -n "${pname}" -a -n "${swap}" ];then
+        test -z "${swap}" && continue
+        if [ "$swap" != '0' -a ${pid} -gt 1 ];then
                 echo -en "$pid\t$swap\t$pname\n"
         fi
 done|grep -E '^[1-9]'|sort -nrk2|\
-awk -F'\t' '{
-    pid[NR]=$1;
-    size[NR]=$2;
-    name[NR]=$3;
-}
-END{
-    for(id=1;id<=length(pid);id++)
-    {
-        if(size[id]<1024)
-            printf("%-5s\t%15sKB\t%s\n",pid[id],size[id],name[id]);
-        else if(size[id]<1048576)
-            printf("%-5s\t%15.2fMB\t%s\n",pid[id],size[id]/1024,name[id]);
-        else
-            printf("%-5s\t%15.2fGB\t%s\n",pid[id],size[id]/1048576,name[id]);
-    }
-}'
+while read pid swap pname
+do
+        swap=`human_read "${swap}"`
+        echo -en "${pid}\t${swap}\t${pname}\n"
+done
