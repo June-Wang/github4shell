a8fc6993aab558d66e76dea11a8e9c4cfbb58466
Alex Wang <qq5910225@gmail.com>
2018-01-17 14:33:29 +0800

    列表方式

--- upload_log2nfs.sh
+++ upload_log2nfs.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-dst_path='/log_bak/trcbak'
+backup_list='/usr/shell/backup.list'
+test -f ${backup_list}||\
+eval "echo ${backup_list} not found!;exit 1"
 
-find /production/vasadm/trc/ -mtime +90 -type f -name "*.trc"|head -n 5000|\
-while read log_file
+cat ${backup_list}|\
+while read src_path dst_path
 do
-        date_time=`stat --format="%y" ${log_file}|xargs -r -i date -d "{}" +"%Y/%m/%d"|head -n1`
-        test -z "${date_time}" && continue
-        path="${dst_path}/${date_time}"
-        test -d ${path} || mkdir -p ${path}
-        gzip ${log_file} && mv ${log_file}.gz ${path}/
+        test -d "${src_path}" || eval "echo ${src_path} not found!;continue"
+        find ${src_path} -mtime +30 -type f|\
+        while read log_file
+        do
+                echo "${log_file}"|grep 'logstash' >/dev/null 2>&1 && rm -f "${log_file}"
+                date_time=`stat --format="%y" ${log_file}|xargs -r -i date -d "{}" +"%Y/%m/"|head -n1`
+                test -z "${date_time}" && continue
+                path="${dst_path}/${date_time}"
+                test -d ${path} || mkdir -p ${path}
+                gzip ${log_file} && mv ${log_file}.gz ${path}/ || mv ${log_file} ${path}/
+        done
 done
