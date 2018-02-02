53d4e1355533de5c5e723da69640fff84b11775c
Alex Wang <qq5910225@gmail.com>
2018-02-02 15:39:06 +0800

    修改src路径

--- install_nginx_ex.sh
+++ install_nginx_ex.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
 yum_server='yum.server.local'
-YUM_PACKAGE='gcc gcc-c++ openssl openssl-devel glib2-devel pcre-devel bzip2-devel'
+yum_pkg='gcc gcc-c++ openssl openssl-devel glib2-devel pcre-devel bzip2-devel'
 
 #SET TEMP DIR
-INSTALL_DIR="/tmp/install_$$"
+tmp_dir="/tmp/install_$$"
+
+#ex pkg dir
+src_dir='/usr/local/src'
 
 #SET EXIT STATUS AND COMMAND
 trap "exit 1"           HUP INT PIPE QUIT TERM
-trap "test -d ${INSTALL_DIR} && rm -rf ${INSTALL_DIR}"  EXIT
+trap "test -d ${tmp_dir} && rm -rf ${tmp_dir}"  EXIT
 
 ls /usr/bin/yum >/dev/null 2>&1 ||\
 eval "echo 未安装yum!;exit 1"
 
-yum --skip-broken --nogpgcheck install -y ${YUM_PACKAGE} > /tmp/yum.log 2>&1 ||\
+yum --skip-broken --nogpgcheck install -y ${yum_pkg} > /tmp/yum.log 2>&1 ||\
 eval "echo yum安装失败!日志请查看: /tmp/yum.log ;exit 1"
 
 download_pkg () {
 local pkg="$1"
 local url="http://${yum_server}/tools/${pkg}"
-test -d ${INSTALL_DIR} || mkdir -p ${INSTALL_DIR}
-wget -q ${url} -O ${INSTALL_DIR}/${pkg} ||\
+test -d ${tmp_dir} || mkdir -p ${tmp_dir}
+wget -q ${url} -O ${tmp_dir}/${pkg} ||\
 eval "echo wget ${url} 下载失败;exit 1"
 }
 
 decompress_pkg () {
 local pkg="$1"
-test -d ${INSTALL_DIR} && cd ${INSTALL_DIR}
+test -d ${tmp_dir} && cd ${tmp_dir}
 test -f ${pkg} && tar xzf ${pkg} ||\
 eval "echo ${pkg}不存在;exit 1"
 }
@@ -34,7 +37,7 @@ eval "echo ${pkg}不存在;exit 1"
 pkg_path () {
 local pkg="$1"
 local pkg_dir=`echo ${pkg}|sed 's/\.tar\.gz//'`
-echo "${INSTALL_DIR}/${pkg_dir}"
+echo "${tmp_dir}/${pkg_dir}"
 }
 
 cd_to_path () {
@@ -68,9 +71,9 @@ download_pkg "${pkg}"
 decompress_pkg "${pkg}"
 pkg_path=`pkg_path "${pkg}"`
 test -d "${pkg_path}" &&\
-mv ${pkg_path} /usr/local/src/
+mv ${pkg_path} ${src_dir}/
 pkg_dir=`echo ${pkg}|sed 's/\.tar\.gz//'`
-lua_nginx_module_path="/usr/local/src/${pkg_dir}"
+lua_nginx_module_path="${src_dir}/${pkg_dir}"
 
 #download nginx-goodies-nginx-sticky-module-ng.tar.gz
 pkg='nginx-goodies-nginx-sticky-module-ng.tar.gz'
@@ -78,9 +81,9 @@ download_pkg "${pkg}"
 decompress_pkg "${pkg}"
 pkg_path=`pkg_path "${pkg}"`
 test -d "${pkg_path}" &&\
-mv ${pkg_path} /usr/local/src/
+mv ${pkg_path} ${src_dir}/
 pkg_dir=`echo ${pkg}|sed 's/\.tar\.gz//'`
-nginx_sticky_module_path="/usr/local/src/${pkg_dir}"
+nginx_sticky_module_path="${src_dir}/${pkg_dir}"
 
 #download ngx_devel_kit-0.2.19.tar.gz
 pkg='ngx_devel_kit-0.2.19.tar.gz'
@@ -88,9 +91,9 @@ download_pkg "${pkg}"
 decompress_pkg "${pkg}"
 pkg_path=`pkg_path "${pkg}"`
 test -d "${pkg_path}" &&\
-mv ${pkg_path} /usr/local/src/
+mv ${pkg_path} ${src_dir}/
 pkg_dir=`echo ${pkg}|sed 's/\.tar\.gz//'`
-ngx_devel_kit_path="/usr/local/src/${pkg_dir}"
+ngx_devel_kit_path="${src_dir}/${pkg_dir}"
 
 #install nginx
 pkg='nginx-1.6.3.tar.gz'
@@ -116,7 +119,7 @@ make install >> /tmp/install_${pkg}.log 2>&1 ||\
 eval "echo 编译nginx失败!日志请查看: /tmp/install_${pkg}.log ;exit 1"
 
 test -d /etc/ld.so.conf.d &&\
-echo '/usr/local/lib' >> /etc/ld.so.conf.d/lua.conf
+echo '/usr/local/lib' > /etc/ld.so.conf.d/ex_module.conf
 
 useradd -s /sbin/nologin nginx
 ln -sf /usr/local/nginx/sbin/nginx  /usr/sbin
