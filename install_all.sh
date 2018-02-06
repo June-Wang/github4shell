e8c7c9b090cdc9a5035743761d1b4dcd20c76008
Alex Wang <qq5910225@gmail.com>
2018-02-06 10:28:43 +0800

    fix

--- install_all.sh
+++ install_all.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-YUM_SERVER='xxx.xxx.xxx.xxx'
+YUM_SERVER='x.x.x.x'
 
 cmds=(
+update_sshd.sh
 init_redhat.sh
 ali_mirrors.sh
 add_history.sh
 add_sysinfo.sh
-install_tripwire.sh
-add_tripwire.sh
 )
 
 for shell in "${cmds[@]}"
 do
         curl http://${YUM_SERVER}/shell/${shell}|/bin/bash
-        echo "${shell}" |grep 'ali_mirrors' >/dev/null 2>&1 &&\
-        yum --skip-broken --nogpgcheck install -y wget
 done
 
-ls /usr/bin/yum >/dev/null 2>&1 && \
-yum --skip-broken --nogpgcheck install -y \
-lrzsz check-mk-agent htop salt-minion mawk vim
-
-#salt
-curl http://${YUM_SERVER}/shell/modify_salt.sh|/bin/bash
+curl http://${YUM_SERVER}/shell/binding_bond.sh > /root/binding_bond.sh
+curl http://${YUM_SERVER}/shell/install_ex.sh > /root/install_ex.sh
