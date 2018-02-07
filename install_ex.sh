8abe109c99cf25181f5734de3c754fdb4a8d2473
Alex Wang <qq5910225@gmail.com>
2018-02-07 10:26:34 +0800

    fix

--- install_ex.sh
+++ install_ex.sh
@@ -2,27 +2,13 @@
 
 YUM_SERVER='yum.server.local'
 
-echo -en '设置vim别名'
-echo -en '\t->\t'
-echo "alias vi='vim'"  > /etc/profile.d/vim_alias.sh
-
-grep -E '^set ts=4' /etc/vimrc >/dev/null 2>&1 ||\
-echo "set nocompatible
-set ts=4
-set backspace=indent,eol,start
-syntax on" >> /etc/vimrc
-echo 'ok'
-
 test -f /usr/bin/yum && \
-yum --skip-broken --nogpgcheck install -y htop salt-minion mawk 
+yum --skip-broken --nogpgcheck install -y htop salt-minion mawk koan check-mk-agent  
 test -f /usr/bin/yum && yum --skip-broken --nogpgcheck update -y
-
-#stop postfix
-chkconfig postfix off
-service postfix stop
 
 cmds=(
 ali_mirrors.sh
+install_nrpe_xinetd.sh
 install_tripwire.sh
 add_tripwire.sh
 update_sshd.sh
@@ -32,3 +18,5 @@ for shell in "${cmds[@]}"
 do
         curl http://${YUM_SERVER}/shell/${shell}|/bin/bash
 done
+
+echo "koan -r --server=${YUM_SERVER} --profile=CentOS6.8-Base-x86_64" > /root/koan.sh
