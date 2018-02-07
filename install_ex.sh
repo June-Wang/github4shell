1c07c2bf9952051d66ffc83da9c69230be61351e
Alex Wang <qq5910225@gmail.com>
2018-02-07 14:29:57 +0800

    fix

--- install_ex.sh
+++ install_ex.sh
@@ -3,7 +3,7 @@
 YUM_SERVER='yum.server.local'
 
 test -f /usr/bin/yum && \
-yum --skip-broken --nogpgcheck install -y htop salt-minion mawk koan check-mk-agent  
+yum --skip-broken --nogpgcheck install -y bc htop salt-minion mawk koan check-mk-agent  
 test -f /usr/bin/yum && yum --skip-broken --nogpgcheck update -y
 
 cmds=(
@@ -11,7 +11,6 @@ ali_mirrors.sh
 install_nrpe_xinetd.sh
 install_tripwire.sh
 add_tripwire.sh
-update_sshd.sh
 )
 
 for shell in "${cmds[@]}"
@@ -20,3 +19,9 @@ do
 done
 
 echo "koan -r --server=${YUM_SERVER} --profile=CentOS6.8-Base-x86_64" > /root/koan.sh
+
+yum_releasever=`rpm -qf /etc/redhat-release|head -n1|xargs -r -i rpm -q --qf %{version} '{}'|sed 's/Server//'`
+
+[ `echo "${yum_releasever} < 7"|bc` -eq 1 ] &&\
+curl http://${YUM_SERVER}/shell/update_sshd.sh|/bin/bash ||\
+exit 0
