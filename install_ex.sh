f122cf87892758ea69a1378b78d8e91a92aa6c94
Alex Wang <qq5910225@gmail.com>
2018-02-07 08:22:30 +0800

    fix

--- install_ex.sh
+++ install_ex.sh
@@ -2,14 +2,29 @@
 
 YUM_SERVER='yum.server.local'
 
-ls /usr/bin/yum >/dev/null 2>&1 && \
-yum --skip-broken --nogpgcheck install -y \
-htop salt-minion mawk
+echo -en '设置vim别名'
+echo -en '\t->\t'
+echo "alias vi='vim'"  > /etc/profile.d/vim_alias.sh
+
+grep -E '^set ts=4' /etc/vimrc >/dev/null 2>&1 ||\
+echo "set nocompatible
+set ts=4
+set backspace=indent,eol,start
+syntax on" >> /etc/vimrc
+echo 'ok'
+
+test -f /usr/bin/yum && \
+yum --skip-broken --nogpgcheck install -y htop salt-minion mawk 
+test -f /usr/bin/yum && yum --skip-broken --nogpgcheck update -y
+
+#stop postfix
+chkconfig postfix off
+service postfix stop
 
 cmds=(
+ali_mirrors.sh
 install_tripwire.sh
 add_tripwire.sh
-id.sh
 update_sshd.sh
 )
 
@@ -17,5 +32,3 @@ for shell in "${cmds[@]}"
 do
         curl http://${YUM_SERVER}/shell/${shell}|/bin/bash
 done
-
-#yum --skip-broken --nogpgcheck update -y
