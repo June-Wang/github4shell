cfe6457a296486ce5a7d1e135144e193c78fb8a1
xiaojun.wang <xiaojun.wang@advance.ai>
2018-11-21 14:26:14 +0800

    fix

--- graylog/alert.sh
+++ graylog/alert.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-graylog_server='1.1.1.1'
-user='user'
-passwd='passwd'
+graylog_server='172.21.194.178'
+user='admin'
+passwd='Advai2018'
 url="http://${graylog_server}:9000/api/search/universal/absolute/export?query=streams%3A"
 streams='5b7d18922994a67297060c19'
+#streams='5b5d91672994a61d81a0b3ae'
 from=`date -d '-2min' +"%F %T"|sed -r 's/ /%20/g;s/:/%3A/g'`
 to=`date -d '-1min' +"%F %T"|sed -r 's/ /%20/g;s/:/%3A/g'`
 fields='message'
@@ -17,10 +18,11 @@ trap "test -f ${tmp} && rm -f ${tmp}; test -f ${tmp}.gz && rm -f ${tmp}.gz"  EXI
 
 cmd="mutt -s ${subj} -e 'set realname=advai_alert'"
 
-curl -u ${user}:${passwd} -s "${url}${streams}&from=${from}&to=${to}&fields=${fields}" > ${tmp}
+curl -u ${user}:${passwd} -s "${url}${streams}&from=${from}&to=${to}&fields=${fields}" > ${tmp} ||\
+eval "echo 请求graylog失败!;exit 1"
 
 test -s ${tmp} && head10=`head -n10 ${tmp}` && num=`wc -l ${tmp}|awk '{print $1}'`
 
 test -s ${tmp} && gzip ${tmp} &&\
 echo -en "${head10}\n共计:${num}条,更多内容详见附件!\n系统自动发送,请勿直接回复.\n"|\
-mutt -s '[mk]web风险请求监控' -e 'set realname=advai_alert' admin@163.com -a ${tmp}.gz
+mutt -s '[mk]finance环境web风险请求监控' -e 'set realname=advai_alert' xiaojun.wang@advance.ai -a ${tmp}.gz
