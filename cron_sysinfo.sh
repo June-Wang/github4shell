65ace3db3d18ae88f279e12d2d0240b45d4f43b5
xiaojun.wang <xiaojun.wang@advance.ai>
2019-03-08 10:46:19 +0800

    修改输出

--- cron_sysinfo.sh
+++ cron_sysinfo.sh
@@ -19,7 +19,7 @@ test -d ${log_path} || mkdir -p ${log_path}
 #[ ${uid} -eq 0 ] && chown nagios.nagios -R ${log_path}
 echo `date -d "now" +"%F %T"` |tee -a ${net_log} ${sys_log} >/dev/null
 
-cpu_utilization=`ps -eo pcpu,args|\
+cpu_utilization=`ps -eo pcpu,rss,vsize,pid,user -eo "%c"|\
 awk '{str="";for (i=2;i<=NF;i++) str=str" "$i;item[str]+=$1}END{for (x in item) if (item[x]>0) print item[x],x}'|\
 sort -nr|\
 tee -a ${sys_log}|\
