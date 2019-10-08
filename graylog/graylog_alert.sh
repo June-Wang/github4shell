c00392e72b42f75e497323194d2e61c27ae61353
xiaojun.wang <xiaojun.wang@advance.ai>
2019-10-08 16:52:53 +0800

    fix

--- graylog/graylog_alert.sh
+++ graylog/graylog_alert.sh
@@ -25,4 +25,4 @@ test -s ${tmp} && head10=`head -n10 ${tmp}` && num=`wc -l ${tmp}|awk '{print $1}
 
 test -s ${tmp} && gzip ${tmp} &&\
 echo -en "${head10}\n共计:${num}条,更多内容详见附件!\n系统自动发送,请勿直接回复.\n"|\
-mutt -s '[mk]finance环境web风险请求监控' -e 'set realname=advai_alert' admin@bj.com -a ${tmp}.gz
+mutt -s '[mk]finance环境web风险请求监控' -e 'set realname=alert' admin@bj.com -a ${tmp}.gz
