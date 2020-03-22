71c44c73a1f3f085a57a660fb905a8e78c5742d5
Alex Wong <5910225@qq.com>
2020-03-23 00:02:32 +0800

    fix

--- install_es6.sh
+++ install_es6.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo apt-get install apt-transport-https
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/oss-6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
-sudo apt-get update && sudo apt-get install elasticsearch-oss
+JAVA_HOME=/data/jdk1.8.0_202
 
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+sudo apt-get update && sudo apt-get install elasticsearch -y
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
 
@@ -16,7 +14,6 @@ es_config='/etc/default/elasticsearch'
 test -f ${es_config} &&\
 sed -r -i '/^DATA_DIR=/d' ${es_config} &&\
 sudo echo 'DATA_DIR=/data/elasticsearch/' >> ${es_config}
-sudo mkdir -p /data/elasticsearch/ && sudo chown -R elasticsearch.elasticsearch /data/elasticsearch/
 
 grep 'vm.max_map_count' /etc/sysctl.conf >/dev/null 2>&1 ||\
 echo 'vm.max_map_count=655360
@@ -49,4 +46,28 @@ gateway.expected_nodes: 3
 gateway.recover_after_time: 5m' > ${es_config}
 test -f ${es_config} && sed -r -i "s/${hostname}/HOSTNAME/g"
 
-sudo apt-get install -y apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
+sudo mkdir -p /data/elasticsearch/ && sudo chown -R elasticsearch.elasticsearch /data/elasticsearch/
+es_default_config='/etc/default/elasticsearch'
+sudo chown elasticsearch.elasticsearch -R /usr/share/elasticsearch/bin/
+sudo chown elasticsearch.elasticsearch ${es_default_config}
+grep -E '^ES_PATH_CONF' ${es_default_config} >/dev/null 2>&1 ||\
+echo 'ES_PATH_CONF=/etc/elasticsearch' >> ${es_default_config}
+
+mydate=`date -d now +"%F_%H-%M-%S"`
+es_config='/etc/elasticsearch/elasticsearch.yml'
+test -f ${es_config} && sudo cp ${es_config} ${es_config}.${mydate}
+
+grep -E '^path.data' ${es_config} >/dev/null 2>&1 ||\
+echo 'path.data: /data/elasticsearch' >> ${es_config}
+grep -E '^path.logs' ${es_config} >/dev/null 2>&1 ||\
+echo 'path.logs: /var/log/elasticsearch' >> ${es_config}
+
+grep -E '^gateway.recover_after_nodes' ${es_config} >/dev/null 2>&1 ||\
+echo 'gateway.recover_after_nodes: 3' >> ${es_config}
+grep -E '^gateway.expected_nodes' ${es_config} >/dev/null 2>&1 ||\
+echo 'gateway.expected_nodes: 3' >> ${es_config}
+grep -E '^gateway.recover_after_time' ${es_config} >/dev/null 2>&1 ||\
+echo 'gateway.recover_after_time: 5m' >> ${es_config}
+
+#sudo apt-get install -y apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
+sudo apt-get install -y apt-transport-https uuid-runtime pwgen
