3f50a7417fd9961e5cb3df9d29a0436ebd1b9b34
Alex Wong <5910225@qq.com>
2020-03-23 10:25:51 +0800

    fix

--- zzz_history.sh
+++ zzz_history.sh
@@ -7,6 +7,7 @@ function log2syslog
 {
    declare command
    command=$BASH_COMMAND
-   logger -p local1.notice -t bash2syslog -i "$HOSTNAME,$USER,$PPID,$SSH_CLIENT,$PWD,$command"
+   IP=`/sbin/ip addr list|grep -oP '\d{1,3}(\.\d{1,3}){3}'|grep -Ev '^127|255$'|head -n1`
+   logger -p local1.notice -t bash2syslog -i "audit_log,$HOSTNAME,$IP,$USER,$PPID,$SSH_CLIENT,$PWD,$command"
 }
 trap log2syslog DEBUG
