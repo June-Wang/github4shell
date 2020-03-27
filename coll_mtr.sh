44855252a4428d93e08c7dd6593b95c18f640722
Alex Wong <5910225@qq.com>
2020-03-27 15:20:02 +0800

    fix

---
+++ coll_mtr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ip="$1"
+
+hostname=`hostname`
+
+mtr='mtr --no-dns -4 --report'
+fields='dst_ip loss   snt   last   avg  best  wrst stdev'
+
+timetamp=`date -d now +"%s%N"`
+
+$mtr ${ip} |grep -v '???'|grep -Ev 'Start|Loss'|\
+awk '{$1="";print}'|\
+while read ${fields}
+do
+    echo "mtr,hostname=${hostname},dst_ip=${dst_ip} loss=${loss},snt=${snt},last=${last},avg=${avg},best=${best},wrst=${wrst},stdev=${stdev} ${timetamp}"
+done|sed 's/%//g'
