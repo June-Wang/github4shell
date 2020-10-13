bb542401653b5bf068041707f4e9614d675cf994
xiaojun.wang <xiaojun.wang@advance.ai>
2020-10-13 10:03:07 +0800

    add

--- install_node_exporter.sh
+++ install_node_exporter.sh
@@ -2,6 +2,7 @@
 
 file='node_exporter-1.0.1.linux-amd64.tar.gz'
 tmp_path="/tmp/node_exporter"
+install_path='/usr/local/prometheus/'
 
 #SET EXIT STATUS AND COMMAND
 trap "exit 1"           HUP INT PIPE QUIT TERM
@@ -13,32 +14,32 @@ mkdir -p ${tmp_path}
 wget http://yum.server.local/tools/${file} -O ${tmp_path}/${file} ||\
 eval "echo wget fail!;exit 1"
 
-test -d /var/lib/prometheus/ ||\
-mkdir -p /var/lib/prometheus/
+test -d ${install_path} ||\
+mkdir -p ${install_path}
 
 cd ${tmp_path} &&\
 tar xzf ${file} 
 
 dir_name=`echo "${file}"|sed 's/.tar.gz//'`
-mv ${dir_name} /var/lib/prometheus/node_exporter
+mv ${dir_name} ${install_path}node_exporter
 
 id prometheus >/dev/null 2>&1 ||\
 useradd --no-create-home -s /bin/false prometheus
 
-chown -R prometheus:prometheus /var/lib/prometheus/node_exporter/
+chown -R prometheus:prometheus ${install_path}node_exporter/
 
 test -f /usr/lib/systemd/system/node_exporter.service||\
-echo '[Unit]
+echo "[Unit]
 Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
 
 [Service]
 User=prometheus
-ExecStart=/var/lib/prometheus/node_exporter/node_exporter
+ExecStart=${install_path}node_exporter/node_exporter
 
 [Install]
-WantedBy=default.target' > /usr/lib/systemd/system/node_exporter.service
+WantedBy=default.target" > /usr/lib/systemd/system/node_exporter.service
 
 systemctl enable --now node_exporter.service
 firewall-cmd --permanent --add-port=9100/tcp
