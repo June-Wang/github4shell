39be0821123bdd8f0fc1058c11f83079fcbae2db
Alex wong <5910225@qq.com>
2020-12-08 14:52:45 +0800

    add

---
+++ install_pinpoint-agent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+pkg='pinpoint-agent-2.2.0.tar.gz'
+path='/usr/local'
+
+test -f ${pkg} ||\
+eval "echo ${pkg} not found!;exit 1"
+
+test -d ${path} ||\
+mkdir -p ${path}
+
+test -f ${pkg} &&\
+tar xzf ${pkg} -C ${path}/
+
+pkg_path=`echo ${pkg}|sed 's/.tar.gz//'`
+
+test -d ${path}/pinpoint-agent ||\
+ln -s ${path}/${pkg_path} ${path}/pinpoint-agent
+
+test -f ${path}/pinpoint-agent/pinpoint.config ||\
+mv ${path}/pinpoint-agent/pinpoint-root.config ${path}/pinpoint-agent/pinpoint.config
+
+echo "vi ${path}/pinpoint-agent/pinpoint.config
+
+detail:
+cd ${path}/pinpoint-agent/profiles
+
+add:
+-javaagent:${path}/pinpoint-agent/pinpoint-bootstrap.jar -Dpinpoint.agentId=app01 -Dpinpoint.applicationName=app-name"
+
