f5b7c671d039719ad03aec9b88a6e2258dd86080
Alex wong <5910225@qq.com>
2020-12-15 10:21:32 +0800

    add

--- install_pinpoint-agent.sh
+++ install_pinpoint-agent.sh
@@ -18,7 +18,8 @@ test -d ${path}/pinpoint-agent ||\
 ln -s ${path}/${pkg_path} ${path}/pinpoint-agent
 
 test -f ${path}/pinpoint-agent/pinpoint.config ||\
-mv ${path}/pinpoint-agent/pinpoint-root.config ${path}/pinpoint-agent/pinpoint.config
+cp pinpoint-springboot.config ${path}/pinpoint-agent/
+#mv ${path}/pinpoint-agent/pinpoint-root.config ${path}/pinpoint-agent/pinpoint.config
 
 echo "vi ${path}/pinpoint-agent/pinpoint.config
 
@@ -27,4 +28,3 @@ cd ${path}/pinpoint-agent/profiles
 
 add:
 -javaagent:${path}/pinpoint-agent/pinpoint-bootstrap.jar -Dpinpoint.agentId=app01 -Dpinpoint.applicationName=app-name"
-
