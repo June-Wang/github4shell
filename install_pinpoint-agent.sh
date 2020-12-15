4cea609ea5e726281037bb9ac891b1640872ceeb
Alex wong <5910225@qq.com>
2020-12-15 10:28:16 +0800

    add

--- install_pinpoint-agent.sh
+++ install_pinpoint-agent.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#包名
 pkg='pinpoint-agent-2.2.0.tar.gz'
+#安装路径
 path='/usr/local'
+#日志服务器ip
+log_server='192.168.100.129'
 
 test -f ${pkg} ||\
 eval "echo ${pkg} not found!;exit 1"
@@ -18,8 +22,10 @@ test -d ${path}/pinpoint-agent ||\
 ln -s ${path}/${pkg_path} ${path}/pinpoint-agent
 
 test -f ${path}/pinpoint-agent/pinpoint.config ||\
-cp pinpoint-springboot.config ${path}/pinpoint-agent/
+cp pinpoint-springboot.config ${path}/pinpoint-agent/pinpoint.config 
 #mv ${path}/pinpoint-agent/pinpoint-root.config ${path}/pinpoint-agent/pinpoint.config
+test -f ${path}/pinpoint-agent/pinpoint.config &&\
+sed -i "s/ip=127.0.0.1/ip=${log_server}/g" ${path}/pinpoint-agent/pinpoint.config 
 
 echo "vi ${path}/pinpoint-agent/pinpoint.config
 
